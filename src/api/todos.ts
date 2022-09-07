66a7fd6a00a457dca79d22fde1d7851338e9b155
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-08 00:41:51 +0300

    Added patch method

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,18 @@
-import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
+
+import { Todo } from '../types/Todo';
+import { RequireAtLeastOne } from '../types/helpers';
+
+// eslint-disable-next-line max-len
+export type PatchTodoData = RequireAtLeastOne<Pick<Todo, 'title' | 'completed'>, 'title' | 'completed'>;
 
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const patchTodo = (
+  todoId: number,
+  data: PatchTodoData,
+) => {
+  return client.patch<Todo>(`/todos/${todoId}`, data);
+};
