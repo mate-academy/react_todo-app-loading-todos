6314badf1cad46775a9a39f2d21812bacd69011a
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-09-28 17:40:49 +0300

    First step

---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+type Props = {
+  setError: (err: boolean) => void;
+};
+
+export const ErrorNotification: React.FC<Props> = ({ setError }) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className="notification is-danger is-light has-text-weight-normal"
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setError(false)}
+      />
+
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
