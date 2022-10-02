c8151a75de420f2b64bf87265502cc9ee59c8c79
Andriy Nosov <nosovandriy@gmail.com>
2022-10-03 00:01:22 +0300

    movedHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, {
-  useState, useContext, useEffect, useRef,
+  useState, useContext, useEffect,
 } from 'react';
 import { AuthContext } from './components/Auth/AuthContext';
 
 import { Todo } from './types/Todo';
 
+import { Header } from './components/Header';
 import { TodoList } from './components/TodoList';
 import { Footer } from './components/Footer';
 import { ErrorNotification } from './components/ErrorNotification';
@@ -14,8 +14,6 @@ import { getTodos } from './api/todos';
 
 export const App: React.FC = () => {
   const user = useContext(AuthContext);
-  const newTodoField = useRef<HTMLInputElement>(null);
-
   const [todos, setTodos] = useState<Todo[]>([]);
   const [filteredBySelect, setFilteredBySelect] = useState(SortType.All);
   const [errorLoad, setErrorLoad] = useState(false);
@@ -49,36 +47,15 @@ export const App: React.FC = () => {
   };
 
   useEffect(() => {
-    // focus the element with `ref={newTodoField}`
-    if (newTodoField.current) {
-      newTodoField.current.focus();
-    }
-
     loadTodosFromServer();
   }, []);
 
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
-
       <div className="todoapp__content">
-        <header className="todoapp__header">
-          <button
-            data-cy="ToggleAllButton"
-            type="button"
-            className="todoapp__toggle-all active"
-          />
 
-          <form>
-            <input
-              data-cy="NewTodoField"
-              type="text"
-              ref={newTodoField}
-              className="todoapp__new-todo"
-              placeholder="What needs to be done?"
-            />
-          </form>
-        </header>
+        <Header />
 
         <TodoList filteredTodo={filteredTodo} />
 
