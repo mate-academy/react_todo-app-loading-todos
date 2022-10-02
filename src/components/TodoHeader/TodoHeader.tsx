408acc747bc0fcb979443d749f13d974c6b1f9ff
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2022-10-02 13:03:02 +0300

    Splited app into components

---
+++ src/components/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,36 @@
+import React, {
+  useEffect,
+  useRef,
+} from 'react';
+
+export const TodoHeader: React.FC = () => {
+  const newTodoField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // focus the element with `ref={newTodoField}`
+    if (newTodoField.current) {
+      newTodoField.current.focus();
+    }
+  }, []);
+
+  return (
+    <header className="todoapp__header">
+      <button
+        data-cy="ToggleAllButton"
+        type="button"
+        className="todoapp__toggle-all active"
+        aria-label="active"
+      />
+
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          ref={newTodoField}
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,33 @@
+export const TodoList: React.FC = () => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      <div
+        data-cy="Todo"
+        className="todo completed"
+      >
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+            defaultChecked
+          />
+        </label>
+
+        <span data-cy="TodoTitle" className="todo__title">HTML</span>
+        <button
+          type="button"
+          className="todo__remove"
+          data-cy="TodoDeleteButton"
+        >
+          ×
+        </button>
+
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    </section>
+  );
+};
