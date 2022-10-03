23f257937d08b58b7bc08977babb286b9c778fa5
Adrian Faryniuk <forever0754@gmail.com>
2022-10-03 16:47:23 +0300

    made file structure

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const ErrorMessage: React.FC = () => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className="notification is-danger is-light has-text-weight-normal"
+    >
+      <button
+        aria-label="Close"
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+      />
+
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
