d757cef3b2fe11ce246d9bf193e65aaf7b3e8aaf
Sergey Pistelev <sergey.pistelev@gmail.com>
2022-10-05 13:26:45 +0300

    TodoItem and TodoList implemented

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todoItem: Todo,
+}
+
+export const TodoItem: React.FC<Props> = ({
+  todoItem,
+}) => {
+  const {
+    title,
+    completed,
+  } = todoItem;
+
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames(
+        'todo',
+        { completed },
+      )}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          defaultChecked
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">{title}</span>
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDeleteButton"
+      >
+        ×
+      </button>
+
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
