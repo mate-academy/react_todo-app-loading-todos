ed0c78681801302b963df997c79b1a083db74a3c
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-10-08 19:32:35 +0300

    Todo app add and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React, {
+  FormEvent,
+  useCallback,
   useContext,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from 'react';
-import { getTodos } from './api/todos';
+import { createTodo, deleteTodo, getTodos } from './api/todos';
 import { AuthContext } from './components/Auth/AuthContext';
-// import { Errors } from './components/Errors';
+import { Errors } from './components/Errors';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { TodoList } from './components/TodoList';
 import { FilterType } from './types/FilterType';
 import { Todo } from './types/Todo';
 
-export function filteredTodo(
+export function filterTodos(
   todos: Todo[],
   filterType: string,
-  query: string,
 ) {
-  const filterBy = todos.filter((todo) => {
+  const filteredTodos = todos.filter((todo) => {
     switch (filterType) {
       case 'completed':
         return todo.completed;
@@ -31,9 +33,7 @@ export function filteredTodo(
     }
   });
 
-  return filterBy.filter(({ title }) => (
-    title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
-  ));
+  return filteredTodos;
 }
 
 export const App: React.FC = () => {
@@ -41,6 +41,12 @@ export const App: React.FC = () => {
   const newTodoField = useRef<HTMLInputElement>(null);
   const [todos, setTodos] = useState<Todo[]>([]);
   const [filteredBy, setFilteredBy] = useState<FilterType>(FilterType.All);
+  const [title, setTitle] = useState('');
+  const [errorText, setErrorText] = useState('');
+  const [selectedId, setSelectedId] = useState<number[]>([]);
+  const [isAdding, setIsAdding] = useState(false);
+
+  const visibleTodos = filterTodos(todos, filteredBy);
 
   useEffect(() => {
     const todosFromServer = async () => {
@@ -59,26 +65,88 @@ export const App: React.FC = () => {
     }
   }, []);
 
+  const addTodo = useCallback(async (event: FormEvent) => {
+    event.preventDefault();
+    if (!title.trim || !user) {
+      setErrorText("You can't do something if you don't know what exactly.");
+
+      return;
+    }
+
+    setIsAdding(true);
+
+    try {
+      const postTodo = await createTodo(title, user.id);
+
+      setTodos((prevTodos) => [...prevTodos, postTodo]);
+    } catch {
+      setErrorText('123');
+    }
+
+    setIsAdding(false);
+    setTitle('');
+  }, [title, user]);
+
+  const removeTodo = useCallback(async (TodoId: number) => {
+    setSelectedId([TodoId]);
+    try {
+      await deleteTodo(TodoId);
+
+      setTodos((prevTodos) => prevTodos.filter(({ id }) => id !== TodoId));
+    } catch {
+      setErrorText('456');
+    }
+  }, [todos, errorText]);
+
+  const completedTodos = useMemo(
+    () => todos.filter(({ completed }) => completed),
+    [todos],
+  );
+
+  const deleteCompletedTodos = useCallback(() => {
+    setSelectedId(completedTodos.map(({ id }) => id));
+
+    Promise.all(completedTodos.map(({ id }) => removeTodo(id)))
+      .then(() => setTodos((prevTodos) => prevTodos
+        .filter(({ completed }) => !completed)))
+      .catch(() => {
+        setErrorText('321');
+        setSelectedId([]);
+      });
+  }, [todos, selectedId, errorText]);
+
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
 
       <div className="todoapp__content">
-        <Header newTodosField={newTodoField} />
-        <TodoList todos={todos} />
+        <Header
+          newTodosField={newTodoField}
+          addNewTodo={addTodo}
+          setTitle={setTitle}
+          title={title}
+        />
 
-        {newTodoField === null
+        {(todos.length > 0 || isAdding)
         && (
-          <Footer
-            todos={todos}
-            filterTypes={setFilteredBy}
-            filterType={filteredBy}
+          <TodoList
+            todos={visibleTodos}
+            removeTodo={removeTodo}
+            selectedId={selectedId}
+            isAdding={isAdding}
           />
         )}
+
+        <Footer
+          todos={todos}
+          setFilter={setFilteredBy}
+          filterStatus={filteredBy}
+          clearCompleted={deleteCompletedTodos}
+        />
       </div>
 
-      {/* {newTodoField === null
-      && <Errors />} */}
+      {errorText
+      && <Errors errorText={errorText} setErrorText={setErrorText} />}
 
     </div>
   );
