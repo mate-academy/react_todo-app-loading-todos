8fadef3fd4297db9bb05c6c2fa2493acf403db73
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-11-07 12:47:08 +0200

    Load todos when the App is loaded

---
+++ src/components/TodosHeader/TodosHeader.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+type Props = {
+  newTodoField: React.RefObject<HTMLInputElement>;
+};
+
+export const TodosHeader: React.FC<Props> = ({ newTodoField }) => (
+  <header className="todoapp__header">
+    <button
+      data-cy="ToggleAllButton"
+      type="button"
+      className="todoapp__toggle-all active"
+    />
+
+    <form>
+      <input
+        data-cy="NewTodoField"
+        type="text"
+        ref={newTodoField}
+        className="todoapp__new-todo"
+        placeholder="What needs to be done?"
+      />
+    </form>
+  </header>
+);
