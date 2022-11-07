61a10a3e8d5bdaaeb054937f3717679e17045597
Pelahiia Khalishkhova <pelahiia.kh@gmail.com>
2022-11-07 14:49:46 +0100

    solution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,67 @@
+import classNames from 'classnames';
+import { FilterTypes } from '../types/FilterTypes';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[],
+  filterType: string,
+  setFilterType: (value: FilterTypes) => void,
+};
+
+export const Footer: React.FC<Props> = ({
+  todos,
+  filterType,
+  setFilterType
+}) => {
+  const todoNumber = todos.filter(todo => !todo.completed).length;
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="todosCounter">
+        {`${todoNumber} items left`}
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          data-cy="FilterLinkAll"
+          href="#/"
+          className={classNames(
+            'filter__link', { selected: FilterTypes.all },
+          )}
+          onClick={() => setFilterType(FilterTypes.all)}
+        >
+          All
+        </a>
+
+        <a
+          data-cy="FilterLinkActive"
+          href="#/active"
+          className={classNames(
+            'filter__link', { selected: FilterTypes.active },
+          )}
+          onClick={() => setFilterType(FilterTypes.active)}
+        >
+          Active
+        </a>
+        <a
+          data-cy="FilterLinkCompleted"
+          href="#/completed"
+          className={classNames(
+            'filter__link', { selected: FilterTypes.completed },
+          )}
+          onClick={() => setFilterType(FilterTypes.completed)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        data-cy="ClearCompletedButton"
+        type="button"
+        className="todoapp__clear-completed"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
