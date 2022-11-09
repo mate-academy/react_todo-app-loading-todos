31cbdaf5715da19262dc7d6a54cf487653304315
Myroslav Diiak <diiakm23819@gmail.com>
2022-11-09 18:44:24 +0200

    fixed solution

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://mate.academy/students-api';
 
-// returns a promise resolved after a given delay
+// a promise resolved after a given delay
 function wait(delay: number) {
   return new Promise(resolve => {
     setTimeout(resolve, delay);
