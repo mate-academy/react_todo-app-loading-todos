d8f214596fe14ea1e16a985eb8fcaf5af672e644
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-11-09 23:20:27 +0200

    implemented filtering, splitted into separate FCs

---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  isError: boolean,
+  setIsError: (isError: boolean) => void,
+};
+
+export const ErrorNotification: React.FC<Props> = ({ isError, setIsError }) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification',
+        'is-danger',
+        'is-light',
+        'has-text-weight-normal',
+        { hidden: !isError },
+      )}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => {
+          setIsError(false);
+        }}
+      />
+
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
