e8faeee6e6758bc39534c1d8ca61a9314039cb03
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-12-09 01:23:53 +0800

    first part

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useState } from 'react';
+import React, {
+  useContext, useEffect, useRef, useState,
+} from 'react';
+import { AuthContext } from './components/Auth/AuthContext';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
 import { Main } from './components/main';
 import { Error } from './components/error';
+import { getTodos } from './api/todos';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // const user = useContext(AuthContext);
-  const [data, setData] = useState('');
+  const user: User | null = useContext(AuthContext);
+  const [data, setData] = useState();
+  const newTodoField = useRef<HTMLInputElement>(null);
 
-  // const handleSubmit = (event: any) => {
-  //   setData(event.target.value);
-  // };
+  // console.log(user);
+  // console.log(user.id);
+
+  useEffect(() => {
+    // focus the element with `ref={newTodoField}`
+    if (newTodoField.current) {
+      newTodoField.current.focus();
+    }
+
+    const fetchTodos = async () => {
+      const todos = await getTodos(user.id);
+
+      setData(todos);
+    };
+
+    fetchTodos();
+  }, []);
+
+  // console.log(dataUrl)
 
   const handleChange = (event: any) => {
     setData(event.target.value);
@@ -26,10 +48,11 @@ export const App: React.FC = () => {
         <Header
           data={data}
           handleChange={handleChange}
+          newTodoField={newTodoField}
           // handleSubmit={handleSubmit}
         />
         {data && (
-          <Main />
+          <Main data={data} />
         )}
 
         {data && (
@@ -42,3 +65,28 @@ export const App: React.FC = () => {
     </div>
   );
 };
+
+// const BASE_URL = 'https://mate.academy/students-api/todos';
+// const request = (url: any) => {
+//   return fetch(`${BASE_URL}${url}`)
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(result => result);
+// };
+
+// const getTodos = () => request('/todos.json');
+
+// getTodos()
+// .then(todos => {
+//   setDataUrl(todos);
+// });
+// .finally(() => {
+//   setIsLoaded(false);
+// });
+// }, []);
+
+// getTodos(user.Id)
+//   .then(todos => {
+//     setData(todos);
+//   });
