be723576463bb14b07f4f6bb022154980e51e7ed
Andriy Shymkiv <winamondezik@gmail.com>
2022-12-12 11:55:07 +0200

    decompose

---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
+export const ErrorNotification: React.FC = () => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className="notification is-danger is-light has-text-weight-normal"
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+      />
+
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
