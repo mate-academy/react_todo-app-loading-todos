5f4294dc1f737bccd85e7178da74cacc28de2430
Danil Serzhantov <sergo.svd@gmail.com>
2022-12-14 20:40:16 +0200

    v1

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames';
+
+type Props = {
+  filter: string,
+  onSetFilter: (filter: string) => void,
+};
+
+export const Filter: React.FC<Props> = ({ filter, onSetFilter }) => {
+  return (
+    <nav className="filter" data-cy="Filter">
+      <a
+        data-cy="FilterLinkAll"
+        href="#/"
+        className={classNames('filter__link',
+          {
+            selected: filter === 'All',
+          })}
+        onClick={() => onSetFilter('All')}
+      >
+        {`${filter}`}
+      </a>
+      <a
+        data-cy="FilterLinkAll"
+        href="#/"
+        className={classNames('filter__link',
+          {
+            selected: filter === 'Active',
+          })}
+        onClick={() => onSetFilter('Active')}
+      >
+        {`${filter}`}
+      </a>
+      <a
+        data-cy="FilterLinkAll"
+        href="#/"
+        className={classNames('filter__link',
+          {
+            selected: filter === 'Complited',
+          })}
+        onClick={() => onSetFilter('Complited')}
+      >
+        {`${filter}`}
+      </a>
+    </nav>
+  );
+};
