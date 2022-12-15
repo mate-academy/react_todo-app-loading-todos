a37f1d16605c4b7348eeb8c592977caca4ec71a5
Zaika Andrii <anzaika13@gmail.com>
2022-12-15 13:07:17 +0200

    filter + other fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,89 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { getTodos } from './api/todos';
 import { AuthContext, AuthProvider } from './components/Auth/AuthContext';
-import { Todos } from './components/Todos/Todos';
+// eslint-disable-next-line max-len
+import { ErrorNotifications } from './components/ErrorNotifications/ErrorNotifications';
+import { TodoFilter } from './components/TodoFilter/TodoFilter';
+import { TodoForm } from './components/TodoForm/TodoForm';
+import { TodoList } from './components/TodoList/TodoList';
+import { Status } from './types/Status';
+import { Todo } from './types/Todo';
+import { Error } from './types/Error';
 
 export const App: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const user = useContext(AuthContext);
-  const newTodoField = useRef<HTMLInputElement>(null);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [status, setStatus] = useState<Status>(Status.All);
+  const [error, setError] = useState<Error>(Error.None);
+
+  const loadTodos = async () => {
+    if (!user) {
+      return;
+    }
+
+    setError(Error.None);
+
+    try {
+      const todosFromServer = await getTodos(user.id);
+
+      setTodos(todosFromServer);
+    } catch {
+      setError(Error.NoTodos);
+    }
+  };
 
   useEffect(() => {
-    // focus the element with `ref={newTodoField}`
-    if (newTodoField.current) {
-      newTodoField.current.focus();
+    loadTodos();
+  }, [user]);
+
+  const todosFilter = () => {
+    switch (status) {
+      case Status.Active:
+        return todos.filter(todo => !todo.completed);
+
+      case Status.Completed:
+        return todos.filter(todo => todo.completed);
+
+      case Status.All:
+      default:
+        return todos;
     }
-  }, []);
+  };
+
+  const visibleTodos = todosFilter();
 
   return (
     <AuthProvider>
-      <Todos />
-      <div
-        data-cy="ErrorNotification"
-        className="notification is-danger is-light has-text-weight-normal"
-      >
-        <button
-          data-cy="HideErrorButton"
-          type="button"
-          className="delete"
-        />
+      <div className="todoapp">
+        <h1 className="todoapp__title">todos</h1>
 
-        Unable to add a todo
-        <br />
-        Unable to delete a todo
-        <br />
-        Unable to update a todo
+        <div className="todoapp__content">
+          <header className="todoapp__header">
+            <button
+              data-cy="ToggleAllButton"
+              type="button"
+              className="todoapp__toggle-all active"
+            />
+            <TodoForm />
+          </header>
+          <TodoList todos={visibleTodos} />
+
+          <footer className="todoapp__footer" data-cy="Footer">
+            <span className="todo-count" data-cy="todosCounter">
+              {`${todos.filter(todo => !todo.completed).length} items left`}
+            </span>
+
+            <TodoFilter
+              status={status}
+              onStatusChange={setStatus}
+            />
+
+          </footer>
+        </div>
+
+        <ErrorNotifications error={error} onErrorMessageChange={setError} />
       </div>
     </AuthProvider>
   );
--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-export const TodoForm: React.FC = ({ }) => {
+export const TodoForm: React.FC = () => {
+  const newTodoField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // focus the element with `ref={newTodoField}`
+    if (newTodoField.current) {
+      newTodoField.current.focus();
+    }
+  }, []);
+
   return (
     <form>
       <input
         data-cy="NewTodoField"
         type="text"
-        // ref={newTodoField}
+        ref={newTodoField}
         className="todoapp__new-todo"
         placeholder="What needs to be done?"
       />
