6a54df57781776d0c5ab0748eedca938dd01317c
Andrii Melnyk <andriimelnyq@gmail.com>
2022-12-28 14:03:53 +0200

    First Solution

---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+import { Errors } from '../../types/Errors';
+
+type Props = {
+  error: Errors,
+  setError: (error: Errors) => void,
+};
+
+export const ErrorNotification = ({ error, setError }: Props) => {
+  setTimeout(() => setError(Errors.NONE), 3000);
+
+  return (
+    <div
+      data-cy="ErrorNotification"
+      // className="notification is-danger is-light has-text-weight-normal"
+      className={
+        classNames(
+          'notification',
+          'is-danger',
+          'is-light',
+          'has-text-weight-normal',
+          { hidden: error === Errors.NONE },
+        )
+      }
+    >
+      <button
+        aria-label="Delete error"
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setError(Errors.NONE)}
+      />
+
+      {error}
+    </div>
+  );
+};
