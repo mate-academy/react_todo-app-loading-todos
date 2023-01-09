ba827e81cffcc9d01b4b38b4bd55dcb04375e66e
John Rust <johnrust12@gmail.com>
2023-01-09 09:55:37 +0200

    Solution 1

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,21 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
+import { SendedTodo } from '../types/SendedTodo';
 
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const sendTodos = (userId: number, data: SendedTodo) => {
+  return client.post<Todo>(`/todos?userId=${userId}`, data);
+};
+
+export const patchTodos = (
+  todoId: number, title?: string, completed?: boolean,
+) => {
+  return client.patch<Todo>(`/todos/${todoId}`, { title, completed });
+};
+
+export const deleteTodos = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
