38e6a059b0d70c01ca468d668eda9786c25b0638
vadiimvooo <ASD4S.Profile@gmail.com>
2023-01-15 13:57:31 +0200

    Solution

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,19 @@
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../Todo/TodoItem';
+
+type Props = {
+  todos: Todo[]
+};
+
+const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+
+      {todos.map(todo => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
+
+export default TodoList;
