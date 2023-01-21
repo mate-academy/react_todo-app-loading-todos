770ad4b40a7da51f90a54335621ac7e831404c45
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-01-21 20:59:16 +0200

    solution

---
+++ src/components/ErrorNotifications.tsx
@@ -0,0 +1,25 @@
+import classNames from "classnames";
+
+type Props = {
+  errorMessage: string;
+};
+
+export const ErrorNotification: React.FC<Props> = ({ errorMessage }) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        { hidden: !errorMessage },
+      )}
+    >
+      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+      />
+      {errorMessage}
+    </div>
+  );
+};
---
+++ src/components/Footer.tsx
@@ -0,0 +1,78 @@
+import classNames from 'classnames';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  filterTodos: string,
+  todosLeft: Todo[],
+  onClickAll: () => void,
+  onClickActive: () => void,
+  onClickCompleted: () => void,
+};
+
+export const Footer: React.FC<Props> = ({
+  filterTodos,
+  todosLeft,
+  onClickAll,
+  onClickActive,
+  onClickCompleted,
+}) => {
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      {filterTodos
+        ? (
+          <span className="todo-count" data-cy="todosCounter">
+            {`${todosLeft.length} items left`}
+          </span>
+        ) : (
+          <span className="todo-count" data-cy="todosCounter">
+            0 items left
+          </span>
+        )}
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          data-cy="FilterLinkAll"
+          href="#/"
+          className={classNames(
+            'filter__link',
+            { selected: filterTodos === 'All' },
+          )}
+          onClick={onClickAll}
+        >
+          All
+        </a>
+
+        <a
+          data-cy="FilterLinkActive"
+          href="#/active"
+          className={classNames(
+            'filter__link',
+            { selected: filterTodos === 'Active' },
+          )}
+          onClick={onClickActive}
+        >
+          Active
+        </a>
+        <a
+          data-cy="FilterLinkCompleted"
+          href="#/completed"
+          className={classNames(
+            'filter__link',
+            { selected: filterTodos === 'Completed' },
+          )}
+          onClick={onClickCompleted}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        data-cy="ClearCompletedButton"
+        type="button"
+        className="todoapp__clear-completed"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
