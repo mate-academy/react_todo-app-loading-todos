9c9b1821660ba46a524b6238bf65aa348f4a4f4c
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-01-23 22:22:52 +0200

    filter and footer was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import React, {
   useRef,
   useState,
 } from 'react';
+import { getTodos } from './api/todos';
 import { AppFooter } from './components/AppFooter/AppFooter';
 import { AppHeader } from './components/AppHeader/AppHeader';
 import { AuthContext } from './components/Auth/AuthContext';
 import { ErrorNotification }
   from './components/ErrorNotification/ErrorNotification';
 import { TodoList } from './components/TodoList/TodoList';
+import { Filter } from './types/Filter';
 import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
-  const [todos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [status, setStatus] = useState<Filter>('All');
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const user = useContext(AuthContext);
@@ -27,6 +30,33 @@ export const App: React.FC = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (user) {
+      getTodos(user.id)
+        .then(setTodos);
+    }
+  });
+
+  const filteredTodos = todos.filter(todo => {
+    switch (status) {
+      case 'All':
+        return todo;
+
+      case 'Active':
+        return !todo.completed;
+
+      case 'Completed':
+        return todo.completed;
+
+      default:
+        throw new Error('Invalid filter type');
+    }
+  });
+
+  const amountOfCompleted = todos.filter(
+    todo => todo.completed,
+  ).length;
+
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
@@ -34,9 +64,15 @@ export const App: React.FC = () => {
       <div className="todoapp__content">
         <AppHeader newTodoField={newTodoField} />
 
-        <TodoList todos={todos} />
+        <TodoList todos={filteredTodos} />
 
-        <AppFooter />
+        {todos.length > 0 && (
+          <AppFooter
+            amountOfCompleted={amountOfCompleted}
+            status={status}
+            changeStatus={setStatus}
+          />
+        )}
       </div>
 
       <ErrorNotification />
--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { SetStateAction } from 'react';
+import { Filter } from '../../types/Filter';
 
-export const AppFooter: React.FC = () => {
+type Props = {
+  amountOfCompleted: number,
+  status: Filter,
+  changeStatus: React.Dispatch<SetStateAction<Filter>>,
+};
+
+export const AppFooter: React.FC<Props> = ({
+  amountOfCompleted,
+  status,
+  changeStatus,
+}) => {
   return (
     <footer className="todoapp__footer" data-cy="Footer">
       <span className="todo-count" data-cy="todosCounter">
-        4 items left
+        {`${amountOfCompleted} items left`}
       </span>
 
       <nav className="filter" data-cy="Filter">
         <a
           data-cy="FilterLinkAll"
           href="#/"
-          className="filter__link selected"
+          className={classNames('filter__link',
+            { selected: status === 'All' })}
+          onClick={() => changeStatus('All')}
         >
           All
         </a>
@@ -19,14 +33,18 @@ export const AppFooter: React.FC = () => {
         <a
           data-cy="FilterLinkActive"
           href="#/active"
-          className="filter__link"
+          className={classNames('filter__link',
+            { selected: status === 'Active' })}
+          onClick={() => changeStatus('Active')}
         >
           Active
         </a>
         <a
           data-cy="FilterLinkCompleted"
           href="#/completed"
-          className="filter__link"
+          className={classNames('filter__link',
+            { selected: status === 'Completed' })}
+          onClick={() => changeStatus('Completed')}
         >
           Completed
         </a>
@@ -36,6 +54,7 @@ export const AppFooter: React.FC = () => {
         data-cy="ClearCompletedButton"
         type="button"
         className="todoapp__clear-completed"
+        onClick={() => changeStatus('All')}
       >
         Clear completed
       </button>
