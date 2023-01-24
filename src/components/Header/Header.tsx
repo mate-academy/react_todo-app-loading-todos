6d6fa3d158ff37964bb333d4434828bbf004c2f7
Olena Yanovska <alena.yn.adw@gmail.com>
2023-01-24 15:50:49 +0200

    split code into components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
+interface Props {
+  newTodoField: React.RefObject<HTMLInputElement>,
+}
+
+export const Header: React.FC<Props> = (props) => {
+  const {
+    newTodoField,
+  } = props;
+
+  return (
+    <header className="todoapp__header">
+      <button
+        data-cy="ToggleAllButton"
+        type="button"
+        className="todoapp__toggle-all active"
+      />
+
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          ref={newTodoField}
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
