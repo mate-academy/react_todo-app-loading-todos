e628432cdcc3949f1e8ae5590260a34752284b70
Vasya Rymarenko <vrima60@gmail.com>
2023-01-31 02:08:21 +0200

    Loading todos was implemented

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { Loader } from '../Loader/Loader';
+
+type Props = {
+  todos: Todo[]
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <div
+          data-cy="Todo"
+          key={todo.id}
+          className={classNames('todo', {
+            completed: todo.completed,
+          })}
+        >
+          <label className="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              className="todo__status"
+              defaultChecked={todo.completed}
+            />
+          </label>
+
+          <span data-cy="TodoTitle" className="todo__title">
+            {todo.title}
+          </span>
+
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDeleteButton"
+          >
+            ×
+          </button>
+
+          <Loader />
+        </div>
+      ))}
+    </section>
+
+  );
+};
