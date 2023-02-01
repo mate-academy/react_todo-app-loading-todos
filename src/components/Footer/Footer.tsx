b631eeb5a5e6bb47fcd61482d1445245375f67e2
Anna Koval <anny.koval@icloud.com>
2023-02-01 22:17:33 +0200

    solution

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,64 @@
+import classnames from 'classnames';
+import { Filter } from '../../types/Filter';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[];
+  filter: Filter;
+  setFilter: (value: Filter) => void;
+};
+
+export const Footer: React.FC<Props> = ({ todos, filter, setFilter }) => {
+  const activeTodos = todos.filter(({ completed }) => !completed).length;
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="todosCounter">
+        {`${activeTodos} items left`}
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          data-cy="FilterLinkAll"
+          href="#/"
+          className={classnames('filter__link',
+            { selected: filter === Filter.ALL })}
+          onClick={() => setFilter(Filter.ALL)}
+        >
+          All
+        </a>
+
+        <a
+          data-cy="FilterLinkActive"
+          href="#/active"
+          className={classnames('filter__link',
+            { selected: filter === Filter.ACTIVE })}
+          onClick={() => setFilter(Filter.ACTIVE)}
+        >
+          Active
+        </a>
+
+        <a
+          data-cy="FilterLinkCompleted"
+          href="#/completed"
+          className={classnames('filter__link', {
+            selected: filter === Filter.COMPLETED,
+          })}
+          onClick={() => setFilter(Filter.COMPLETED)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {activeTodos && (
+        <button
+          data-cy="ClearCompletedButton"
+          type="button"
+          className="todoapp__clear-completed"
+        >
+          Clear completed
+        </button>
+      )}
+    </footer>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+type Props = {
+  newTodoField: React.RefObject<HTMLInputElement>
+};
+
+export const Header: React.FC<Props> = ({ newTodoField }) => (
+  <header className="todoapp__header">
+    <button
+      data-cy="ToggleAllButton"
+      type="button"
+      className="todoapp__toggle-all active"
+      aria-label="toggle All"
+    />
+
+    <form>
+      <input
+        data-cy="NewTodoField"
+        type="text"
+        ref={newTodoField}
+        className="todoapp__new-todo"
+        placeholder="What needs to be done?"
+      />
+    </form>
+  </header>
+);
