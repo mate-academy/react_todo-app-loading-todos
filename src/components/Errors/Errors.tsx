dc86fc46d12293c0375a2266a2901042431fa21c
Saleh BUD <saleh.7734@gmail.com>
2023-02-07 07:38:07 +0200

    Errors Component

--- src/components/Errors/Errors.tsx
+++ src/components/Errors/Errors.tsx
@@ -1,16 +1,38 @@
-/* Notification is shown in case of any error */
-/* Add the 'hidden' class to hide the message smoothly */
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
-export const Errors = () => {
+export const Errors: React.FC = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true);
+    }, 3000);
+  }, []);
+
+  const onCloseClick = () => {
+    setVisible(true);
+  };
+
   return (
-    <div className="notification is-danger is-light has-text-weight-normal">
-      {/* <button type="button" className="delete" /> */}
+    <div
+      className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        { visible },
+      )}
+    >
+      <button
+        type="button"
+        className="delete"
+        aria-label="Close error"
+        onClick={onCloseClick}
+      />
       {/* show only one message at a time */}
       Unable to add a todo
-      {/* <br />
+      <br />
       Unable to delete a todo
       <br />
-      Unable to update a todo */}
+      Unable to update a todo
     </div>
   );
 };
