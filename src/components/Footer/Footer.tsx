ddbbb95d95f03836fa1338fd7d761510737dfc54
Avichez <starkavichi@gmail.com>
2023-02-13 21:27:52 +0200

    solution

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import cn from 'classnames';
+import { FilterBy } from '../../types/FilterBy';
+
+type Props = {
+  quantity: number,
+  filterBy: FilterBy,
+  setFilterBy: (filter: FilterBy) => void,
+  hasCompletedTodos: boolean,
+};
+
+export const Footer: React.FC<Props> = ({
+  quantity,
+  filterBy,
+  setFilterBy,
+  hasCompletedTodos,
+}) => {
+  return (
+    <footer className="todoapp__footer">
+      <span className="todo-count">
+        {`${quantity} items left`}
+      </span>
+
+      {/* Active filter should have a 'selected' class */}
+      <nav className="filter">
+        <a
+          href="#/"
+          className={cn('filter__link', {
+            selected: filterBy === FilterBy.ALL,
+          })}
+          onClick={() => setFilterBy(FilterBy.ALL)}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={cn('filter__link', {
+            selected: filterBy === FilterBy.ACTIVE,
+          })}
+          onClick={() => setFilterBy(FilterBy.ACTIVE)}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={cn('filter__link', {
+            selected: filterBy === FilterBy.COMPLETED,
+          })}
+          onClick={() => setFilterBy(FilterBy.COMPLETED)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {/* don't show this button if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        disabled={!hasCompletedTodos}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import cn from 'classnames';
+
+type Props = {
+  hasActiveTodos: boolean,
+};
+
+export const Header: React.FC<Props> = ({ hasActiveTodos }) => {
+  const [title, setTitle] = useState('');
+
+  return (
+    <header className="todoapp__header">
+      {/* this buttons is active only if there are some active todos */}
+      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      <button
+        type="button"
+        className={cn('todoapp__toggle-all', {
+          active: !hasActiveTodos,
+        })}
+      />
+
+      {/* Add a todo on form submit */}
+      <form>
+        <input
+          type="text"
+          value={title}
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </form>
+    </header>
+  );
+};
