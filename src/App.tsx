b4dd97d6ad1b419f386e3a9b24b1cadb8d08137d
Kareem Ashkar <kareemalachkar@gmail.com>
2023-02-14 02:22:10 +0300

    final

--- src/App.tsx
+++ src/App.tsx
@@ -98,13 +98,6 @@ export const App: React.FC = () => {
       {/* Add the 'hidden' class to hide the message smoothly */}
       <div className="notification is-danger is-light has-text-weight-normal">
         <button type="button" className="delete" />
-
-        {/* show only one message at a time */}
-        {/* Unable to add a todo
-        <br />
-        Unable to delete a todo
-        <br />
-        Unable to update a todo */}
       </div>
     </div>
   );
