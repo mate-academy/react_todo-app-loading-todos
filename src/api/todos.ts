4c08b21fb526dceba99c482207cb1416ab37118a
volkan yildiz <yildizvolkan91@gmail.com>
2023-02-20 14:56:15 +0300

    status changed

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,18 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const getTodos = (userId: number) => {
+const USER_ID = 6142; // 6357
+
+export const getTodos = (userId: number = USER_ID) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
+};
+
+export const createTodo = (title: string) => {
+  return client.post<Todo>('/todos', {
+    title,
+    completed: false,
+    userId: USER_ID,
+  });
 };
 
 // Add more methods here
