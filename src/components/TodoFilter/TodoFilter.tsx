70f86dd0cbf6faa487fcc67b60538da00e18a369
Anton Vasyliev <magisterol963@gmail.com>
2023-02-20 18:10:57 +0200

    Implemented todos loading, errors and filtering.

---
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -0,0 +1,40 @@
+import cn from 'classnames';
+import { Filter } from '../../enum/Filter';
+
+type Props = {
+  filter: Filter,
+  setFilter: (value: Filter) => void;
+};
+
+export const TodoFilter: React.FC<Props> = ({ filter, setFilter }) => {
+  return (
+    <nav className="filter">
+      <a
+        href="#/"
+        className={cn('filter__link',
+          { selected: filter === Filter.ALL })}
+        onClick={() => setFilter(Filter.ALL)}
+      >
+        All
+      </a>
+
+      <a
+        href="#/active"
+        className={cn('filter__link',
+          { selected: filter === Filter.ACTIVE })}
+        onClick={() => setFilter(Filter.ACTIVE)}
+      >
+        Active
+      </a>
+
+      <a
+        href="#/completed"
+        className={cn('filter__link',
+          { selected: filter === Filter.COMPLETED })}
+        onClick={() => setFilter(Filter.COMPLETED)}
+      >
+        Completed
+      </a>
+    </nav>
+  );
+};
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,19 @@
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main">
+      {todos.map(todo => (
+        <TodoInfo
+          todo={todo}
+          key={todo.id}
+        />
+      ))}
+    </section>
+  );
+};
