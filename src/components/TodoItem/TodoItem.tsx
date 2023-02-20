0038b8efaa227ec11b615a20a12b0b009164fa46
Anton Zhytonbaiev <chipchilinka11@gmail.com>
2023-02-20 21:01:32 +0200

    version 1.0

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+};
+
+export const TodoItem: React.FC<Props> = ({ todo }) => (
+  <>
+    {todo.completed ? (
+      <div className="todo completed">
+        <label className="todo__status-label">
+          <input
+            type="checkbox"
+            className="todo__status"
+            checked
+          />
+        </label>
+
+        <span className="todo__title">{todo.title}</span>
+
+        {/* Remove button appears only on hover */}
+        <button
+          type="button"
+          className="todo__remove"
+        >
+          ×
+        </button>
+
+        {/* overlay will cover the todo while it is being updated */}
+        <div className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    ) : (
+      <div className="todo">
+        <label className="todo__status-label">
+          <input
+            type="checkbox"
+            className="todo__status"
+          />
+        </label>
+
+        <span className="todo__title">{todo.title}</span>
+        <button type="button" className="todo__remove">×</button>
+
+        <div className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    )}
+  </>
+);
