0cc7b4f044b97bf18c410065ca1f4b68c78bdc4b
Vlad Boikov <vladboikov95@hotmail.com>
2023-02-20 23:27:36 +0200

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
-import classNames from 'classnames';
 import { UserWarning } from './UserWarning';
 import { Todo } from './types/Todo';
 import { getTodos } from './api/todos';
 import { Filter } from './types/Filter';
 import { Error } from './types/Error';
+import { TodosList } from './components/TodosList';
+import { Footer } from './components/Footer';
+import { ErrorNotifications } from './components/ErrorNotifications';
+import { Header } from './components/Header';
 
 const USER_ID = 6335;
 
@@ -13,15 +16,13 @@ export const App: React.FC = () => {
   const [allTodos, setTodos] = useState<Todo[]>([]);
   const [isError, setIsError] = useState<Error | null>(null);
   const [filter, setFilter] = useState<Filter>(Filter.All);
-  const [isErrorHidden, setIsErrorHidden] = useState(true);
 
   useEffect(() => {
     getTodos(USER_ID)
       .then(setTodos)
       .catch(() => {
         setIsError(Error.Load);
-        setIsErrorHidden(false);
-        setTimeout(() => setIsErrorHidden(true), 3000);
+        setTimeout(() => setIsError(null), 3000);
       });
   }, []);
 
@@ -36,7 +37,7 @@ export const App: React.FC = () => {
     }
   };
 
-  const activeTodos = allTodos.filter(todo => !todo.completed);
+  const activeTodos = filterHandler(allTodos, Filter.Active);
   const completedTodos = filterHandler(allTodos, Filter.Completed);
   const visibleTodos = filterHandler(allTodos, filter);
 
@@ -49,122 +50,29 @@ export const App: React.FC = () => {
       <h1 className="todoapp__title">todos</h1>
 
       <div className="todoapp__content">
-        <header className="todoapp__header">
-          <button
-            type="button"
-            className={classNames(
-              'todoapp__toggle-all', { active: activeTodos.length === 0 },
-            )}
-            style={{ opacity: allTodos.length === 0 ? 0 : 1 }}
+        <Header
+          allTodos={allTodos}
+          activeTodos={activeTodos}
+        />
 
-          />
+        {!!allTodos.length && (
+          <>
+            <TodosList todos={visibleTodos} />
 
-          {/* Add a todo on form submit */}
-          <form>
-            <input
-              type="text"
-              className="todoapp__new-todo"
-              placeholder="What needs to be done?"
+            <Footer
+              activeTodos={activeTodos}
+              completedTodos={completedTodos}
+              filter={filter}
+              setFilter={setFilter}
             />
-          </form>
-        </header>
-
-        <section className="todoapp__main">
-          {visibleTodos.map(todo => (
-            <div
-              key={todo.id}
-              className={classNames('todo', { completed: todo.completed })}
-            >
-              <label className="todo__status-label">
-                <input
-                  type="checkbox"
-                  className="todo__status"
-                  checked={todo.completed}
-                />
-              </label>
-
-              <span className="todo__title">{todo.title}</span>
-
-              <button type="button" className="todo__remove">×</button>
-
-              <div className="modal overlay">
-                <div className="modal-background has-background-white-ter" />
-                <div className="loader" />
-              </div>
-            </div>
-          ))}
-        </section>
-
-        {allTodos.length > 0 && (
-          <footer className="todoapp__footer">
-            <span className="todo-count">
-              {`${activeTodos.length} items left`}
-            </span>
-
-            <nav className="filter">
-              <a
-                href="#/"
-                className={classNames(
-                  'filter__link', { selected: filter === Filter.All },
-                )}
-                onClick={() => setFilter(Filter.All)}
-              >
-                All
-              </a>
-
-              <a
-                href="#/active"
-                className={classNames(
-                  'filter__link', { selected: filter === Filter.Active },
-                )}
-                onClick={() => setFilter(Filter.Active)}
-              >
-                Active
-              </a>
-
-              <a
-                href="#/completed"
-                className={classNames(
-                  'filter__link', { selected: filter === Filter.Completed },
-                )}
-                onClick={() => setFilter(Filter.Completed)}
-              >
-                Completed
-              </a>
-            </nav>
-
-            <button
-              type="button"
-              className="todoapp__clear-completed"
-              style={{ opacity: completedTodos.length === 0 ? 0 : 1 }}
-            >
-              Clear completed
-            </button>
-          </footer>
+          </>
         )}
       </div>
 
-      {isError && (
-        <div className={classNames(
-          'notification is-danger is-light has-text-weight-normal',
-          { hidden: isErrorHidden },
-        )}
-        >
-          <button
-            type="button"
-            className="delete"
-            onClick={() => setIsErrorHidden(true)}
-          />
-
-          {isError === Error.Load && 'Unable to load todos'}
-          <br />
-          {isError === Error.Add && 'Unable to add a todo'}
-          <br />
-          {isError === Error.Delete && 'Unable to delete a todo'}
-          <br />
-          {isError === Error.Update && 'Unable to update a todo'}
-        </div>
-      )}
+      <ErrorNotifications
+        isError={isError}
+        setIsError={setIsError}
+      />
     </div>
   );
 };
