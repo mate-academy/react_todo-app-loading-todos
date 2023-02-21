d7ac495159042ae6ca073982229b1389c190e41f
maasiimka <mrozisalovec@gmail.com>
2023-02-21 12:49:45 +0200

    added memoization for some variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { getTodos } from './api/todos';
 import { ErrorMessage } from './components/Error';
 import { Footer } from './components/Footer';
@@ -17,38 +17,42 @@ export const App: React.FC = () => {
   const [status, setStatus] = useState<Status>(Status.All);
   const [error, setError] = useState<Error>(Error.NONE);
 
+  const fetchTodos = async () => {
+    const todosFromServer = await getTodos(USER_ID)
+      .catch(() => {
+        setError(Error.FETCH);
+
+        setTimeout(() => {
+          setError(Error.NONE);
+        }, 3000);
+      });
+
+    if (todosFromServer) {
+      setTodos(todosFromServer);
+    }
+  };
+
   useEffect(() => {
-    const fetchTodos = async () => {
-      const todosFromServer = await getTodos(USER_ID)
-        .catch(() => {
-          setError(Error.FETCH);
-
-          setTimeout(() => {
-            setError(Error.NONE);
-          }, 3000);
-        });
-
-      if (todosFromServer) {
-        setTodos(todosFromServer);
-      }
-    };
-
     fetchTodos();
   }, []);
+
+  const todosByStatus = useMemo(() => (
+    status === Status.All
+      ? todos
+      : todos.filter(({ completed }) => (
+        status === Status.COMPLETED ? completed : !completed
+      ))
+  ), [status, todos]);
+
+  const completedCount = useMemo(() => (
+    todos.reduce((acc, todo) => (
+      todo.completed ? acc : acc + 1
+    ), 0)
+  ), [todos]);
 
   if (!USER_ID) {
     return <UserWarning />;
   }
-
-  const todosByStatus = status === Status.All
-    ? todos
-    : todos.filter(({ completed }) => (
-      status === Status.COMPLETED ? completed : !completed
-    ));
-
-  const completedCount = todos.reduce((acc, todo) => (
-    todo.completed ? acc : acc + 1
-  ), 0);
 
   return (
     <div className="todoapp">
@@ -58,16 +62,14 @@ export const App: React.FC = () => {
         <Header />
         {
           todos.length !== 0 && (
-            <TodoList todos={todosByStatus} />
-          )
-        }
-        {
-          todos.length !== 0 && (
-            <Footer
-              completedCount={completedCount}
-              status={status}
-              setStatus={setStatus}
-            />
+            <>
+              <TodoList todos={todosByStatus} />
+              <Footer
+                completedCount={completedCount}
+                status={status}
+                setStatus={setStatus}
+              />
+            </>
           )
         }
       </div>
