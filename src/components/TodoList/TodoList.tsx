cd4c14a38c97485e1214120e2d6838cbff60a854
Vasyl Pavlenko <genlus@ukr.net>
2023-03-15 10:54:30 +0200

    Some fixes after request

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -6,41 +6,37 @@ type Props = {
   todos: Todo[],
 };
 
-export const TodoList: React.FC<Props> = ({ todos }) => {
-  return (
-    <section className="todoapp__main">
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="todoapp__main">
 
-      {todos.map(({ title, completed }) => {
-        return (
-          <div className={classNames(
-            'todo', { completed: completed === true },
-          )}
-          >
-            <label className="todo__status-label">
-              <input
-                type="checkbox"
-                className="todo__status"
-                checked={completed}
-              />
-            </label>
+    {todos.map(({ title, completed }) => (
+      <div className={classNames(
+        'todo', { completed: completed === true },
+      )}
+      >
+        <label className="todo__status-label">
+          <input
+            type="checkbox"
+            className="todo__status"
+            checked={completed}
+          />
+        </label>
 
-            <span className="todo__title">{title}</span>
+        <span className="todo__title">{title}</span>
 
-            <button
-              type="button"
-              className="todo__remove"
-            >
-              ×
-            </button>
+        <button
+          type="button"
+          className="todo__remove"
+        >
+          ×
+        </button>
 
-            <div className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
-        );
-      })}
+        <div className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    ))}
 
-    </section>
-  );
-};
+  </section>
+);
