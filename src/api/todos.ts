be27674cea0303aaf58f678f994d9cc50d1f16a4
Vitalii Skomskiy <skomskiy@ukr.net>
2023-03-28 20:51:08 +0300

    commit

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,6 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const getTodos = (userId: number) => {
-  return client.get<Todo[]>(`/todos?userId=${userId}`);
+export const getTodos = (userId: number, filter?: boolean) => {
+  return client.get<Todo[]>(`/todos?userId=${userId}${filter ? `&completed=${filter}` : ''}`);
 };
-
-// Add more methods here
