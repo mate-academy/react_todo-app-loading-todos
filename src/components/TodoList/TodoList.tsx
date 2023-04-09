c6964f94dd66694d0d5286c38b6c7d23befd43b0
Vladyslav Mosor <mosorvlad@gmail.com>
2023-04-09 09:42:49 +0200

    separate component for Todo created

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
-import classNames from 'classnames';
 import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
 
 type Props = {
   todos: Todo[]
@@ -9,36 +9,8 @@ type Props = {
 export const TodoList: FC<Props> = ({ todos }) => {
   return (
     <section className="todoapp__main">
-      {todos.map(({ id, title, completed }) => (
-        <div
-          className={classNames(
-            'todo',
-            {
-              completed,
-            },
-          )}
-          key={id}
-        >
-          <label className="todo__status-label">
-            <input
-              type="checkbox"
-              className="todo__status"
-              checked={completed}
-            />
-          </label>
-
-          <span className="todo__title">
-            {title}
-          </span>
-          <button type="button" className="todo__remove">
-            ×
-          </button>
-
-          <div className="modal overlay">
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
-        </div>
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
       ))}
     </section>
   );
