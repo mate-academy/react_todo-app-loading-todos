4d5c273e596bb357d98640aec298384d5aea7b6a
Olecsiy <infernoregent@gmail.com>
2023-04-10 19:09:14 +0300

    solution

---
+++ src/components/TodoFilter.tsx
@@ -0,0 +1,52 @@
+import { FC } from 'react';
+import classNames from 'classnames';
+import { StatusToFilterBy, Todo } from '../types/Todo';
+
+interface Props {
+  filterBy: StatusToFilterBy,
+  setFilterBy: (status: StatusToFilterBy) => void;
+  itemsLeft: number;
+  todos: Todo[];
+}
+
+export const TodoFilter: FC<Props> = ({
+  filterBy,
+  setFilterBy,
+  itemsLeft,
+  todos,
+}) => {
+  const filterByLink = Object.entries(StatusToFilterBy);
+
+  return (
+    <footer className="todoapp__footer">
+      <span className="todo-count">
+        {`${itemsLeft} items left`}
+      </span>
+
+      {/* Active filter should have a 'selected' class */}
+      <nav className="filter">
+        {filterByLink.map(([label, value]) => {
+          return (
+            <a
+              href="#/"
+              className={classNames('filter__link', {
+                selected: filterBy === label,
+              })}
+              key={value}
+              onClick={() => setFilterBy(value)}
+            >
+              {label}
+            </a>
+          );
+        })}
+      </nav>
+
+      {itemsLeft !== todos.length && (
+        <button type="button" className="todoapp__clear-completed">
+          Clear completed
+        </button>
+      )}
+
+    </footer>
+  );
+};
