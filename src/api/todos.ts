98079c74cbe0d8ca68eb2786c30946661730e1d0
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-04-11 09:24:31 +0300

    Solution todo without filter

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,16 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const addTodo = (userId: number, data: any) => {
+  return client.post<Todo[]>(`/todos?userId=${userId}`, data);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const updateTodo = (userId: number, id: number, data: any) => {
+  return client.patch(`/todos?userId=${userId}/${id}`, data);
+};
+
+export const deleteTodo = (userId: number, id: number) => {
+  return client.delete(`/todos?userId=${userId}/${id}`);
+};
