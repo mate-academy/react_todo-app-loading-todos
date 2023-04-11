d5f934a35323adb7fbe84dedf28ed567bd6c74e7
Sergey Lipikhin <football3@ukr.net>
2023-04-11 12:54:01 +0300

    Remove redundant list items

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
 import { TodoItem } from '../TodoItem';
-import { TodoEditForm } from '../TodoEditForm';
-import { TodoLoadingOverlay } from '../TodoLoadingOverlay';
 
 type Props = {
   todos: Todo[];
@@ -17,35 +15,6 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
           todo={todo}
         />
       ))}
-
-      {/* This todo is being edited */}
-      <div className="todo">
-        <label className="todo__status-label">
-          <input
-            type="checkbox"
-            className="todo__status"
-          />
-        </label>
-
-        <TodoEditForm />
-
-        <div className="modal overlay">
-          <div className="modal-background has-background-white-ter" />
-          <div className="loader" />
-        </div>
-      </div>
-
-      {/* This todo is in loadind state */}
-      <div className="todo">
-        <label className="todo__status-label">
-          <input type="checkbox" className="todo__status" />
-        </label>
-
-        <span className="todo__title">Todo is being saved now</span>
-        <button type="button" className="todo__remove">×</button>
-
-        <TodoLoadingOverlay />
-      </div>
     </section>
   );
 };
