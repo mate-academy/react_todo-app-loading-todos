4d4c555c9d8efe67fd6f060cf89603b594d8f797
Ilya <ukraintsev17@gmail.com>
2023-04-13 15:12:50 +0300

    Solution

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { TodoStatus } from '../../types/TodoStatus';
+
+type Props = {
+  todos: Todo[],
+  todoStatus: TodoStatus,
+  setTodoStatus: (status: TodoStatus) => void,
+};
+
+export const Footer: React.FC<Props> = ({
+  todos,
+  todoStatus,
+  setTodoStatus,
+}) => {
+  const itemsLeft = todos.filter((todo) => !todo.completed).length;
+
+  return (
+    <footer className="todoapp__footer">
+      <span className="todo-count">
+        {`${itemsLeft} items left`}
+      </span>
+
+      <nav className="filter">
+        <a
+          href="#/"
+          className={classNames(
+            'filter__link',
+            {
+              selected: todoStatus === TodoStatus.ALL,
+            },
+          )}
+          onClick={() => setTodoStatus(TodoStatus.ALL)}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames(
+            'filter__link',
+            {
+              selected: todoStatus === TodoStatus.ACTIVE,
+            },
+          )}
+          onClick={() => setTodoStatus(TodoStatus.ACTIVE)}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames(
+            'filter__link',
+            {
+              selected: todoStatus === TodoStatus.COMPLETED,
+            },
+          )}
+          onClick={() => setTodoStatus(TodoStatus.COMPLETED)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
