b0bf2fb8968f8151870a36ded5a52c2a1aab2dd9
Maksym Huriev <makspsih.73@gmail.com>
2023-04-21 12:12:01 +0300

    react_todo-app-loading-todos

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,8 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
+export const USER_ID = 9929;
+
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
-
-// Add more methods here
