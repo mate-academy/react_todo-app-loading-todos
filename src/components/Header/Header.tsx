5affd0145ff2b3a3335f90f5a7bd36a75fe1c1e6
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-05-06 11:18:14 +0300

    Comments were removed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,13 +14,11 @@ export const Header: React.FC<Props> = ({ hasSomeTodos, onChangeIsError }) => {
 
   return (
     <header className="todoapp__header">
-      {/* this buttons is active only if there are some active todos */}
       {hasSomeTodos && (
         // eslint-disable-next-line jsx-a11y/control-has-associated-label
         <button type="button" className="todoapp__toggle-all active" />
       )}
 
-      {/* Add a todo on form submit */}
       <form onSubmit={(event) => handleFormSubmit(event)}>
         <input
           type="text"
