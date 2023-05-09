4dc728839230ee8176fa6e9ec1fb45345fc19206
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-05-09 14:42:18 +0300

    React Todo app Loading todos v.0.2 - filterType added, filters position fixed

--- src/App.tsx
+++ src/App.tsx
@@ -91,7 +91,9 @@ export const App: React.FC = () => {
               className="delete"
               onClick={() => setError(Errors.NONE)}
             />
+
             {error}
+
           </div>
         )}
     </div>
