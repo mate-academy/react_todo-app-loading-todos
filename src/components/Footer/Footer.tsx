9be3a007f49609cb07ce510aa08c18f0399da6f1
Artem Kyrylenko <kyrylenko.a.a@gmail.com>
2023-05-12 15:22:11 +0300

    solution

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Status } from '../../enum/Status';
+
+interface Props {
+  onStatusSelect: (status: string) => void;
+  todoStatus: string;
+}
+
+export const Footer: React.FC<Props> = ({ onStatusSelect, todoStatus }) => {
+  const handleClick = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    onStatusSelect(event.currentTarget.text.toLowerCase());
+  };
+
+  return (
+    <footer className="todoapp__footer">
+      <span className="todo-count">
+        3 items left
+      </span>
+
+      {/* Active filter should have a 'selected' class */}
+      <nav className="filter">
+        <a
+          href="#/"
+          className={classNames(
+            'filter__link',
+            { selected: todoStatus === Status.All },
+          )}
+          onClick={handleClick}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames(
+            'filter__link',
+            { selected: todoStatus === Status.Active },
+          )}
+          onClick={handleClick}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames(
+            'filter__link',
+            { selected: todoStatus === Status.Completed },
+          )}
+          onClick={handleClick}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {/* don't show this button if there are no completed todos */}
+      <button type="button" className="todoapp__clear-completed">
+        Clear completed
+      </button>
+    </footer>
+  );
+};
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todos: Todo[]
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main">
+      {/* This is a completed todo */}
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className={classNames(
+            'todo',
+            { completed: todo.completed },
+          )}
+        >
+          <label className="todo__status-label">
+            <input
+              type="checkbox"
+              className="todo__status"
+              checked={todo.completed}
+            />
+          </label>
+
+          <span className="todo__title">{todo.title}</span>
+
+          {/* Remove button appears only on hover */}
+          <button type="button" className="todo__remove">×</button>
+
+          {/* overlay will cover the todo while it is being updated */}
+          <div className="modal overlay">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
