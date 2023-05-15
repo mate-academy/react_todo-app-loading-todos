76a0889617dcf1bec729f428a9e0941d62007e53
Dima <dmitriy.movchan.02@gmail.com>
2023-05-15 15:32:02 +0300

    Add other files to commit

---
+++ src/TodoMain/TodoList.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+interface Props {
+  visibleTodos: Todo[]
+}
+
+export const TodoList: React.FC<Props> = ({ visibleTodos }) => {
+  return (
+    <section className="todoapp__main">
+      {visibleTodos.map(todo => (
+        <TodoItem todo={todo} />
+      ))}
+    </section>
+  );
+};
