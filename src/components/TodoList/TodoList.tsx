24d63d5d4cc059e34c66f2f5919e6d491a87a6de
Rage1502 <karaulchenkoilja@gmail.com>
2023-05-15 09:26:53 +0300

    finish

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,36 @@
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => (
+  <section className="todoapp__main">
+    {todos.map((todo) => {
+      return (
+        <div
+          className={cn('todo', {
+            completed: todo.completed,
+          })}
+          key={todo.id}
+        >
+          <label className="todo__status-label">
+            <input type="checkbox" className="todo__status" checked />
+          </label>
+
+          <span className="todo__title">{todo.title}</span>
+
+          <button type="button" className="todo__remove">
+            ×
+          </button>
+
+          <div className="modal overlay">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div>
+        </div>
+      );
+    })}
+  </section>
+);
