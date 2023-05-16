20cc531f6f31a1adada78d9f09900d20c6882835
Pasha <pashamalyshkin2003@gmail.com>
2023-05-16 19:51:33 +0300

    deleted return statement

--- src/Components/TodoForm/TodoForm.tsx
+++ src/Components/TodoForm/TodoForm.tsx
@@ -5,16 +5,14 @@ interface Props {
   onChangeTitle: (title: string) => void;
 }
 
-export const TodoForm: FC<Props> = memo(({ title, onChangeTitle }) => {
-  return (
-    <form>
-      <input
-        type="text"
-        className="todoapp__new-todo"
-        placeholder="What needs to be done?"
-        value={title}
-        onChange={(event) => onChangeTitle(event.target.value)}
-      />
-    </form>
-  );
-});
+export const TodoForm: FC<Props> = memo(({ title, onChangeTitle }) => (
+  <form>
+    <input
+      type="text"
+      className="todoapp__new-todo"
+      placeholder="What needs to be done?"
+      value={title}
+      onChange={(event) => onChangeTitle(event.target.value)}
+    />
+  </form>
+));
