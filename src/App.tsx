4c312d61892229e24ead81c501b5c3cd324dadbf
zynkd <coding707@mailbox.org>
2023-05-24 11:27:26 +0200

    Removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
-// Types
 import { Todo } from './types/Todo';
 import { FilterOption } from './types/FilterOption';
-// Functions
 import { getTodos } from './api/todos';
-// Components
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Alert } from './components/Alert';
@@ -12,7 +9,6 @@ import { UserWarning } from './UserWarning';
 import { TodoList } from './components/TodoList';
 
 const USER_ID = 10527;
-// TODO endpoint: https://mate.academy/students-api/todos?userId=10527
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -68,13 +64,11 @@ export const App: React.FC = () => {
 
         {todos.length > 0 && <TodoList todos={visibleTodos} />}
 
-        {/* Hides the footer if there are no todos */}
         {todos.length > 0 && (
           <Footer todos={todos} filter={filter} setFilter={setFilter} />
         )}
       </div>
 
-      {/* Shows Alert if there are errors */}
       {hasError && <Alert />}
     </div>
   );
