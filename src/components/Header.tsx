b2da2120d1381964679488b92b67c9efef5ab23c
Evgen Maximov <e19861025@gmail.com>
2023-05-25 14:56:00 +0300

    Solution

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Header: React.FC = () => (
+  <header className="todoapp__header">
+    <button
+      type="button"
+      aria-label="All"
+      className="todoapp__toggle-all active"
+    />
+    <form>
+      <input
+        type="text"
+        className="todoapp__new-todo"
+        placeholder="What needs to be done?"
+      />
+    </form>
+  </header>
+);
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TodoInfo } from './TodoInfo';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[],
+};
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main">
+      {todos.map((todo) => (
+        <TodoInfo
+          key={todo.id}
+          todo={todo}
+        />
+      ))}
+    </section>
+  );
+};
