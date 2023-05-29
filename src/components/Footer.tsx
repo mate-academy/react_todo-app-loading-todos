ad3c87729a89fbf5d03d5b7897fc121d990c5ff9
Gabriella Ivanjuk <gabriella.ivanjuk@gmail.com>
2023-05-29 22:54:30 +0300

    solution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import classNames from 'classnames';
+import { ClearCompleted } from './ClearCompleted';
+import { Filter } from '../enums/enums';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[];
+  filterValue: Filter;
+  onActive: () => void;
+  onAll: () => void;
+  onCompleted: () => void;
+};
+
+export const Footer: React.FC<Props> = ({
+  todos,
+  filterValue,
+  onActive,
+  onAll,
+  onCompleted,
+}) => (
+  <footer className="todoapp__footer">
+    <span className="todo-count">
+      {`${todos.length} items left`}
+    </span>
+
+    {/* Active filter should have a 'selected' class */}
+    <nav className="filter">
+      <a
+        href="#/"
+        className={classNames('filter__link', {
+          selected: filterValue === Filter.All,
+        })}
+        onClick={onAll}
+      >
+        All
+      </a>
+
+      <a
+        href="#/active"
+        className={classNames('filter__link', {
+          selected: filterValue === Filter.Active,
+        })}
+        onClick={onActive}
+      >
+        Active
+      </a>
+
+      <a
+        href="#/completed"
+        className={classNames('filter__link', {
+          selected: filterValue === Filter.Completed,
+        })}
+        onClick={onCompleted}
+      >
+        Completed
+      </a>
+    </nav>
+
+    <ClearCompleted todos={todos} />
+  </footer>
+);
