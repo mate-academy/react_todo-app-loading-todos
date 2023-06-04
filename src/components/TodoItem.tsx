d88316c74d91f3b1e7ab0d76767876f59b493f16
marshalek37 <patrykmarp@gmai.com>
2023-06-04 20:11:30 +0200

    Fix

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,36 +3,37 @@ import classNames from 'classnames';
 import { Todo } from '../types/Todo';
 
 interface TodoItemProps {
-  todo: Todo,
+  todo: Todo;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo }) => (
-  <div
-    key={todo.id}
-    className={classNames('todo', { completed: todo.completed })}
-  >
-    <label className="todo__status-label">
-      <input
-        type="checkbox"
-        className="todo__status"
-        checked={todo.completed}
-      />
-    </label>
+const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const { id, completed, title } = todo;
 
-    <span className="todo__title">{todo.title}</span>
-
-    <button
-      type="button"
-      className="todo__remove"
+  return (
+    <div
+      key={id}
+      className={classNames('todo', { completed })}
     >
-      ×
-    </button>
+      <label className="todo__status-label">
+        <input
+          type="checkbox"
+          className="todo__status"
+          checked={completed}
+        />
+      </label>
 
-    <div className="modal overlay">
-      <div className="modal-background has-background-white-ter" />
-      <div className="loader" />
+      <span className="todo__title">{title}</span>
+
+      <button type="button" className="todo__remove">
+        ×
+      </button>
+
+      <div className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TodoItem;
