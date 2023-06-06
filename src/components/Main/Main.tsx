55056651c783e4416e6bda0f09821337e31d0acc
Vladyslav-Fedorishchev <fedorischev23@gmail.com>
2023-06-06 13:07:34 +0300

    Solution

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todos: Todo[],
+}
+
+export const Main: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main">
+      {todos.map(todo => (
+        <div
+          key={todo.id}
+          className={classNames(
+            'todo',
+            { completed: todo.completed },
+          )}
+        >
+          <label className="todo__status-label">
+            <input
+              type="checkbox"
+              className="todo__status"
+              checked
+            />
+          </label>
+
+          <span className="todo__title">{todo.title}</span>
+
+          <button type="button" className="todo__remove">×</button>
+
+          <div className="modal overlay">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
