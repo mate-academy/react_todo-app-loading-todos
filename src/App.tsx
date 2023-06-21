dac963533b4d0220f3967453cc966b453b4e7684
Kocherha Roman <f.your.d.2017@gmail.com>
2023-06-21 12:40:05 +0300

    fixed all commets from mantor

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@
 import React, { useState, useEffect } from 'react';
 import { UserWarning } from './UserWarning';
 import { Loader } from './components/Loader';
-import { TodoFilter, FilterTypes } from './components/TodoFilter';
+import { FilterTypes } from './components/TodoFilter';
 import { Todos } from './components/Todos';
-
 import { client } from './utils/fetchClient';
-
 import { Todo } from './types/Todo';
+import { Footer } from './components/Footer';
 
 const USER_ID = '10682';
 
-const TODO_ERRORS = {
-  add: ' Unable to add a todo',
-  delete: 'Unable to delete a todo',
-  update: 'Unable to update a todo',
-};
+export enum TodoErros {
+  add = ' Unable to add a todo',
+  delete = 'Unable to delete a todo',
+  update = 'Unable to update a todo',
+}
 
 export const App: React.FC = () => {
   if (!USER_ID) {
@@ -40,6 +39,12 @@ export const App: React.FC = () => {
     );
   }, []);
 
+  const handleImputTodo = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setInput(e.target.value);
+  };
+
   const handleAddTodo = (
     e: React.KeyboardEvent<HTMLElement>,
   ) => {
@@ -53,21 +58,12 @@ export const App: React.FC = () => {
       client.post(USER_ID, newTodo).then((todo) => {
         setTodos((prevTodos) => [...prevTodos, todo as Todo]);
         setInput('');
-        if (error && error === TODO_ERRORS.add) {
+
+        if (error && error === TodoErros.add) {
           setError('');
         }
-      }).catch(() => setError(TODO_ERRORS.add));
+      }).catch(() => setError(TodoErros.add));
     }
-  };
-
-  const handleCreateTodo = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setInput(e.target.value);
-  };
-
-  const removeTodo = (todoId: number) => {
-    setTodos(prevTodos => prevTodos.filter((todo) => todo.id !== todoId));
   };
 
   const filteredTodos = filter === FilterTypes.All
@@ -96,17 +92,15 @@ export const App: React.FC = () => {
 
       <div className="todoapp__content">
         <header className="todoapp__header">
-          {/* this buttons is active only if there are some active todos */}
           <button type="button" className="todoapp__toggle-all active" />
 
-          {/* Add a todo on form submit */}
           <form onSubmit={preventSubmit}>
             <input
               type="text"
               className="todoapp__new-todo"
               placeholder="What needs to be done?"
               value={input}
-              onChange={handleCreateTodo}
+              onChange={handleImputTodo}
               onKeyDown={handleAddTodo}
             />
           </form>
@@ -116,21 +110,17 @@ export const App: React.FC = () => {
           ? <Loader />
           : (
             <Todos
-              onRemove={removeTodo}
               todos={filteredTodos}
             />
           )}
-        {/* Hide the footer if there are no todos */}
         {todos.length ? (
-          <TodoFilter
+          <Footer
             todos={todos}
             onFilterType={filterTodos}
           />
         ) : undefined}
       </div>
 
-      {/* Notification is shown in case of any error */}
-      {/* Add the 'hidden' class to hide the message smoothly */}
       {error && (
         <div className="notification is-danger is-light has-text-weight-normal">
           <button type="button" className="delete" />
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Todo } from '../../types/Todo';
-
 export enum FilterTypes {
   All = 'all',
   Active = 'active',
@@ -9,54 +7,37 @@ export enum FilterTypes {
 }
 
 type Props = {
-  todos: Todo[]
   onFilterType: (type: FilterTypes) => void
 };
 
 export const TodoFilter: React.FC<Props> = ({
-  todos,
   onFilterType,
 }) => {
   return (
-    <footer className="todoapp__footer">
-      <span className="todo-count">
-        {`${todos.length} items left`}
-      </span>
+    <nav className="filter">
+      <a
+        href="#/"
+        className="filter__link"
+        onClick={() => onFilterType(FilterTypes.All)}
+      >
+        All
+      </a>
 
-      {/* Active filter should have a 'selected' class */}
-      <nav className="filter">
-        <a
-          href="#/"
-          className="filter__link"
-          onClick={() => onFilterType(FilterTypes.All)}
-          // value={FilterTypes.All}
-        >
-          All
-        </a>
+      <a
+        href="#/active"
+        className="filter__link"
+        onClick={() => onFilterType(FilterTypes.Active)}
+      >
+        Active
+      </a>
 
-        <a
-          href="#/active"
-          className="filter__link"
-          onClick={() => onFilterType(FilterTypes.Active)}
-          // value={FilterTypes.Active}
-        >
-          Active
-        </a>
-
-        <a
-          href="#/completed"
-          className="filter__link"
-          onClick={() => onFilterType(FilterTypes.Completed)}
-          // value={FilterTypes.Completed}
-        >
-          Completed
-        </a>
-      </nav>
-
-      {/* don't show this button if there are no completed todos */}
-      <button type="button" className="todoapp__clear-completed">
-        Clear completed
-      </button>
-    </footer>
+      <a
+        href="#/completed"
+        className="filter__link"
+        onClick={() => onFilterType(FilterTypes.Completed)}
+      >
+        Completed
+      </a>
+    </nav>
   );
 };
