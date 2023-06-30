acab46568b63d16431aabb105d7e88d9c3f7c1c5
Anna Holovko <anutkagolovko1472@gmail.com>
2023-06-30 18:17:53 +0300

    solution

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import cn from 'classnames';
+
+interface Props {
+  error: string | null;
+  onCloseError: () => void;
+}
+
+/* eslint-disable jsx-a11y/control-has-associated-label */
+export const Error: FC<Props> = ({ error, onCloseError }) => {
+  return (
+    <div className={cn('notification', 'is-danger',
+      'is-light', 'has-text-weight-normal', { hidden: !error })}
+    >
+      <button
+        type="button"
+        className="delete"
+        onClick={onCloseError}
+      />
+      {/* show only one message at a time */}
+      {error}
+      {/* show only one message at a time */}
+      {/* Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo */}
+    </div>
+  );
+};
