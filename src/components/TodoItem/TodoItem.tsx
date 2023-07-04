604bf7c8792ee57245698bd216ac418ad11e35c5
Victor P <vpanya97@gmail.com>
2023-07-04 17:37:44 +0300

    Solution

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoItem: React.FC<Props> = ({ todo }) => {
+  return (
+    <div className={cn('todo', {
+      completed: todo.completed,
+    })}
+    >
+      <label className="todo__status-label">
+        <input
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+        />
+      </label>
+
+      <span className="todo__title">{todo.title}</span>
+
+      {/* Remove button appears only on hover */}
+      <button type="button" className="todo__remove">×</button>
+
+      {/* overlay will cover the todo while it is being updated */}
+      <div className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { TodoItem } from '../TodoItem/TodoItem';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main">
+      {todos.map(todo => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
