cf4145a350f2db54a77cc9dcbf37489c5b4b2399
volodymyr.tkachenko <vladimirscloud@icloud.com>
2023-07-05 21:39:01 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export const App: React.FC = () => {
 
   useEffect(() => {
     getTodos(USER_ID)
-      .then((todosFromServer: Todo[]) => setTodos(todosFromServer))
+      .then((setTodos))
       .catch(() => setError('Unable to get todos'));
   }, []);
 
