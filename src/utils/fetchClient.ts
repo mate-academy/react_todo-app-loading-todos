3cf6995e07364a61ffee0acb03648602eac03443
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-07-27 14:49:06 +0300

    fixed BASE_URL

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const BASE_URL = 'https://matse.academy/students-api';
+const BASE_URL = 'https://mate.academy/students-api';
 
 function wait(delay: number) {
   return new Promise(resolve => {
