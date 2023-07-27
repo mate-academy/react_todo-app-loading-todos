8ca89899e55b552c02300080f4bbbe97f45f0dec
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-07-27 21:47:49 +0300

    fix linter

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -4,7 +4,11 @@ export const TodoHeader: React.FC = () => {
   return (
     <header className="todoapp__header">
       {/* this buttons is active only if there are some active todos */}
-      <button type="button" className="todoapp__toggle-all active" />
+      {/* eslint-disable-next-line */}
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+      />
 
       {/* Add a todo on form submit */}
       <form>
--- src/components/TodoNotification.tsx
+++ src/components/TodoNotification.tsx
@@ -23,7 +23,7 @@ export const TodoNotification: React.FC<Props> = ({
         hidden: !hasError,
       })}
     >
-
+      {/* eslint-disable-next-line */}
       <button
         type="button"
         className="delete"
