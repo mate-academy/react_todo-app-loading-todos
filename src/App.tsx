4534e72e80c9a55ebec15a19f01baf2bd9439980
ihorhladkov <ihor.hladkov@gmail.com>
2023-07-28 17:15:38 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,29 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
-import classNames from 'classnames';
 import { UserWarning } from './UserWarning';
 import { createTodo, getTodos } from './api/todos';
 import { Todo } from './types/Todo';
 import { TodoFooter } from './components/TodoFooter';
 import { TodoList } from './components/TodoList';
 import { TodoAppHeader } from './components/TodoAppHeader';
-
-const USER_ID = 11206;
-
-const prepearedTodos = (
-  todos: Todo[],
-  filterType: string,
-) => {
-  const todosCopy = [...todos].filter(todo => {
-    switch (filterType) {
-      case 'all':
-        return todos;
-      case 'active':
-        return !todo.completed;
-      case 'completed':
-        return todo.completed;
-      default:
-        return 0;
-    }
-  });
-
-  return todosCopy;
-};
+import { utils } from './utils/variables';
+import { ErrorNotification } from './components/ErrorNotification';
+import { ErrorTypes } from './types/ErrorTypes';
 
 export const App: React.FC = () => {
+  const { prepearedTodos, USER_ID } = utils;
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTitle, setNewTitle] = useState('');
-  const [hasTitleError, setHasTitleError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const [filterType, setFilterType] = useState('all');
   const todoList = prepearedTodos(todos, filterType);
 
   useEffect(() => {
-    getTodos(USER_ID).then(setTodos);
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage(ErrorTypes.loadError);
+      });
   }, []);
 
   if (!USER_ID) {
@@ -48,31 +33,22 @@ export const App: React.FC = () => {
   const addTodo = (
     event: React.FormEvent,
   ) => {
-    if (newTitle.trim().length === 0) {
-      setHasTitleError(true);
+    event.preventDefault();
 
-      setTimeout(() => {
-        setHasTitleError(false);
-      }, 3000);
+    if (newTitle.trim().length === 0) {
+      setErrorMessage(ErrorTypes.emptyError);
 
       return;
     }
 
-    event.preventDefault();
-
     createTodo({ title: newTitle, completed: false, userId: USER_ID })
       .then(newTodo => {
         setTodos(currentTodos => [...currentTodos, newTodo]);
+        setNewTitle('');
       })
-      .catch((error) => {
-        setHasTitleError(true);
-        setTimeout(() => {
-          setHasTitleError(false);
-        }, 3000);
-        throw error;
+      .catch(() => {
+        setErrorMessage(ErrorTypes.submitError);
       });
-
-    setNewTitle('');
   };
 
   return (
@@ -98,28 +74,12 @@ export const App: React.FC = () => {
         ) : null}
       </div>
 
-      <div
-        className={classNames(
-          'notification',
-          'is-danger',
-          'is-light',
-          'has-text-weight-normal',
-          {
-            hidden: !hasTitleError,
-          },
-        )}
-      >
-        <button type="button" className="delete" />
-
-        {newTitle.trim().length === 0 && (
-          'Title can`t be empty'
-        )}
-
-        {newTitle.trim().length !== 0 && (
-          'Unable to add a todo'
-        )}
-        <br />
-      </div>
+      {errorMessage && (
+        <ErrorNotification
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+        />
+      )}
     </div>
   );
 };
