2b7fb95edf49db0367310a2ed27410f6e94cbde7
Artem <artem.ivashchenko.exe@gmail.com>
2023-08-06 18:35:59 +0300

    solution

---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import classNames from 'classnames';
+import React from 'react';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoHeader: React.FC<Props> = ({ todos }) => {
+  const isSomeVisible = todos.length !== 0;
+  const isEveryCompleted = todos.every(todo => todo.completed);
+
+  return (
+    <header className="todoapp__header">
+      {isSomeVisible && (
+        <button
+          type="button"
+          className={classNames('todoapp__toggle-all', {
+            active: isEveryCompleted,
+          })}
+        />
+      )}
+
+      {/* Add a todo on form submit */}
+      <form>
+        <input
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
