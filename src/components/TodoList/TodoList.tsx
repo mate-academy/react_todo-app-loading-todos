19602cd3e9dc7791ab42b4ccb7cf7e3a9e918206
Diana Tuz <dianatuz12@gmail.com>
2023-08-12 21:48:57 +0300

    todo-app-loading

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -21,64 +21,66 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
 
   return (
 
-    todos.map(todo => {
-      const { id, title, completed } = todo;
+    <>
+      {todos.map(todo => {
+        const { id, title, completed } = todo;
 
-      return (
-        <div
-          className={classNames('todo', {
-            completed,
-            editing: isEditing,
-          })}
-          key={id}
-        >
-          <label
-            className="todo__status-label"
-            onDoubleClick={handleDoubleClick}
-            htmlFor={`toggle-view-${id}`}
+        return (
+          <div
+            className={classNames('todo', {
+              completed,
+              editing: isEditing,
+            })}
+            key={id}
           >
-            <input
-              type="checkbox"
-              className="todo__status"
-              id={`toggle-view-${id}`}
-              checked={checked}
-              onClick={handleChecked}
-            />
-          </label>
-
-          <span className="todo__title">{title}</span>
-
-          <button type="button" className="todo__remove">×</button>
-
-          {/* overlay will cover the todo while it is being updated */}
-
-          <div className="modal overlay">
-            <div
-              className="modal-background has-background-white-ter"
-            />
-            <div className="loader" />
-          </div>
-
-          <div className="todo">
-            <label className="todo__status-label">
-              <input type="checkbox" className="todo__status" />
+            <label
+              className="todo__status-label"
+              onDoubleClick={handleDoubleClick}
+              htmlFor={`toggle-view-${id}`}
+            >
+              <input
+                type="checkbox"
+                className="todo__status"
+                id={`toggle-view-${id}`}
+                checked={checked}
+                onClick={handleChecked}
+              />
             </label>
 
-            <span className="todo__title">
-              Todo is being saved now
-            </span>
+            <span className="todo__title">{title}</span>
+
             <button type="button" className="todo__remove">×</button>
 
-            {/* 'is-active' class puts this modal on top of the todo */}
-            <div className="modal overlay is-active">
+            {/* overlay will cover the todo while it is being updated */}
+
+            <div className="modal overlay">
               <div
                 className="modal-background has-background-white-ter"
               />
               <div className="loader" />
             </div>
+
+            <div className="todo">
+              <label className="todo__status-label">
+                <input type="checkbox" className="todo__status" />
+              </label>
+
+              <span className="todo__title">
+                Todo is being saved now
+              </span>
+              <button type="button" className="todo__remove">×</button>
+
+              {/* 'is-active' class puts this modal on top of the todo */}
+              <div className="modal overlay is-active">
+                <div
+                  className="modal-background has-background-white-ter"
+                />
+                <div className="loader" />
+              </div>
+            </div>
           </div>
-        </div>
-      );
-    })
+        );
+      })}
+    </>
   );
 };
