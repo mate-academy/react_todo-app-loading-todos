aa4c11627f39d842efcf13dad33e8d2dbfa88c6d
haduigon <haduigon@gmail.com>
2023-09-03 19:06:17 +0300

    first commit

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable */
+import { Todo } from "../types/Todo";
+import classNames  from "classnames";
+
+type Props = {
+  todo: Todo,
+}
+export const TodoItem: React.FC<Props> = ({ todo }) => {
+  function handleCheck() {}
+  console.log(todo.completed, 'todo');
+
+  return (
+    <div className={classNames('todo',{
+      'completed': todo.completed
+    })}>
+    <label className="todo__status-label">
+      <input
+        type="checkbox"
+        className="todo__status"
+        // checked={todo.completed}
+        onChange={handleCheck}
+      />
+    </label>
+
+    <span className="todo__title">{todo.title}</span>
+
+    {/* Remove button appears only on hover */}
+    <button type="button" className="todo__remove">×</button>
+
+    {/* overlay will cover the todo while it is being updated */}
+    <div className="modal overlay">
+      <div className="modal-background has-background-white-ter" />
+      <div className="loader" />
+    </div>
+  </div>
+  )
+}
---
+++ src/components/TodoContext.tsx
@@ -0,0 +1,136 @@
+/* eslint-disable */
+import React, {
+  Dispatch,
+  useEffect,
+  useReducer,
+} from "react";
+import { Todo } from "../types/Todo";
+import { getTodos } from '../api/todos';
+
+const USER_ID = 11384;
+
+export enum ACTIONS {
+  SORT,
+  SET_LENGTH,
+  SET_LIST,
+  SET_ERROR,
+}
+
+export enum FILTER {
+  ALL = 'All',
+  ACTIVE = 'Active',
+  COMPLETED = 'Completed',
+}
+
+type Action = { type: ACTIONS.SORT, payload: string }
+  | { type: ACTIONS.SET_LIST, payload: Todo[] }
+  | { type: ACTIONS.SET_LENGTH, payload: number }
+  | { type: ACTIONS.SET_ERROR, payload: string }
+
+interface Data {
+  list: Todo[],
+  sortBy: string,
+  totalLength: number,
+  error: string,
+};
+
+type State = [
+  state: Data,
+  dispatch: Dispatch<Action>,
+];
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case ACTIONS.SORT:
+      return [
+        {
+          ...state[0],
+          sortBy: action.payload,
+        },
+        () => { }
+      ];
+    case ACTIONS.SET_LIST:
+      return [
+        {
+          ...state[0],
+          list: action.payload,
+        },
+        () => { }
+      ];
+    case ACTIONS.SET_ERROR:
+      return [
+        {
+          ...state[0],
+          error: action.payload,
+        },
+        () => { }
+      ];
+    case ACTIONS.SET_LENGTH:
+      return [
+        {
+          ...state[0],
+          totalLength: action.payload,
+        },
+        () => { }
+      ];
+    default:
+      return state;
+  }
+}
+
+const initialState: State = [
+  {
+    list: [],
+    sortBy: 'All',
+    totalLength: 0,
+    error: '',
+  },
+  () => { },
+];
+
+export const StateContext = React.createContext(initialState);
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const TodoProvider: React.FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(res => {
+        dispatch({ type: ACTIONS.SET_LIST, payload: res })
+        dispatch({ type: ACTIONS.SET_LENGTH, payload: res.length })
+      })
+      .catch(() => dispatch({ type: ACTIONS.SET_ERROR, payload: 'Can`t load the page' }))
+  }, []);
+
+  function filteredTodos(): Todo[] {
+    switch (state[0].sortBy) {
+      case FILTER.ALL:
+        return [...state[0].list];
+      case FILTER.COMPLETED:
+        return state[0].list.filter(todo => todo.completed);
+      case FILTER.ACTIVE:
+        return state[0].list.filter(todo => !todo.completed);
+      default:
+        return state[0].list;
+    }
+  }
+
+  return (
+    <StateContext.Provider value={[{
+      ...state[0],
+      list: filteredTodos(),
+    }, dispatch]}>
+      {children}
+    </StateContext.Provider>
+  )
+
+}
+
+
+
+
+
