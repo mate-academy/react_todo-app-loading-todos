42d4dc4f033c4ee03813b8b2c593ec7928ed3f4e
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-09-18 19:04:25 +0300

    split on components

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
+export const Error = () => {
+  return (
+    // Notification is shown in case of any error
+    // Add the 'hidden' class to hide the message smoothly
+    <div className="notification is-danger is-light has-text-weight-normal">
+      <button type="button" className="delete" />
+
+      {/* show only one message at a time */}
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
