b064beb3640dc35108551e266d3ee07a235f3689
Andrii Kuz <andrykuz4556@gmail.com>
2023-09-19 17:46:34 +0300

    Solved task

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { Filter } from '../../types/Filter';
+
+type Props = {
+  todos: Todo[];
+  filter: Filter;
+  setFilter: (value: Filter) => void;
+};
+
+export const Footer: React.FC<Props> = ({ todos, filter, setFilter }) => {
+  return (
+    <footer className="todoapp__footer">
+      <span className="todo-count">
+        {`${todos.length} items left`}
+      </span>
+
+      {/* Active filter should have a 'selected' class */}
+      <nav className="filter">
+        {Object.keys(Filter).map((key) => {
+          const value = Filter[key as keyof typeof Filter];
+
+          return (
+            <a
+              key={key}
+              href={`#/${value}`}
+              className={classNames('filter__link', {
+                selected: value === filter,
+              })}
+              onClick={() => setFilter(value)}
+            >
+              {key}
+            </a>
+          );
+        })}
+        {/* <a href="#/" className="filter__link selected">
+          All
+        </a>
+
+        <a href="#/active" className="filter__link">
+          Active
+        </a>
+
+        <a href="#/completed" className="filter__link">
+          Completed
+        </a> */}
+      </nav>
+
+      {/* don't show this button if there are no completed todos */}
+      <button type="button" className="todoapp__clear-completed">
+        Clear completed
+      </button>
+    </footer>
+  );
+};
