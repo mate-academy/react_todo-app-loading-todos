98bf9e3e1f49eb4b669a58c343e8880b35529c79
Roman Yatsykovych <55942779+yar0man@users.noreply.github.com>
2023-09-20 16:08:57 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useMemo, useState } from 'react';
 import classNames from 'classnames';
-
 import { UserWarning } from './UserWarning';
 import { Todo } from './types/Todo';
 import { getTodos } from './api/todos';
