76e5954d7f0b2366159c7f0c0d301a0b95798892
Anton Herasymov <antonherasymov2002@gmail.com>
2023-09-20 14:52:11 +0300

    added components for Todo,added filter and erros for todos

---
+++ src/components/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,28 @@
+type Props = {
+  activeTodosCount: number,
+};
+
+export const TodoHeader: React.FC<Props> = ({ activeTodosCount }) => {
+  return (
+    <header className="todoapp__header">
+      {!!activeTodosCount && (
+        // eslint-disable-next-line jsx-a11y/control-has-associated-label
+        <button
+          type="button"
+          className="todoapp__toggle-all active"
+          data-cy="ToggleAllButton"
+        />
+      )}
+
+      {/* Add a todo on form submit */}
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
