078049cac877b52d60f6bf70cd7bdf84d2fff293
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-09-20 19:34:03 +0300

    add task solution

---
+++ src/context/TodoContext.tsx
@@ -0,0 +1,93 @@
+import React, {
+  ReactNode, useEffect, useMemo, useState,
+} from 'react';
+import { Todo } from '../types/Todo';
+import { getTodos } from '../api/todos';
+
+export const USER_ID = 11552;
+
+export enum FilterOption {
+  all = 'all',
+  active = 'active',
+  completed = 'completed',
+}
+
+export enum ErrorOption {
+  FetchErr = 'Unable to upload todos',
+  AddError = 'Unable to add a todo',
+  DeleteError = 'Unable to delete a todo',
+  UpdateError = 'Unable to update a todo',
+}
+
+type Props = {
+  children: ReactNode;
+};
+
+interface ContextValues {
+  todos: Todo[],
+  visibleTodos: Todo[],
+  activeTodosAmount: number,
+  error: string | null,
+  filter: string,
+  setError: React.Dispatch<React.SetStateAction<ErrorOption | null>>,
+  setFilter: React.Dispatch<React.SetStateAction<FilterOption>>,
+}
+
+export const TodoContext = React.createContext({} as ContextValues);
+
+export const TodoProvider: React.FC<Props> = ({ children }) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<FilterOption>(FilterOption.all);
+  const [error, setError] = useState<ErrorOption | null>(null);
+
+  useEffect(() => {
+    setError(null);
+
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => setError(ErrorOption.FetchErr));
+  }, []);
+
+  const filterTodos = (
+    array: Todo[], selectedFilterOption: FilterOption,
+  ) => {
+    const filteredTodos = array.filter(todo => {
+      switch (selectedFilterOption) {
+        case FilterOption.active:
+          return !todo.completed;
+        case FilterOption.completed:
+          return todo.completed;
+        default:
+          return todo;
+      }
+    });
+
+    return filteredTodos;
+  };
+
+  const visibleTodos: Todo[] = useMemo(() => filterTodos(todos, filter),
+    [todos, filter]);
+
+  const activeTodosAmount = todos.filter(todo => !todo.completed).length;
+
+  const contextValues: ContextValues = useMemo(() => ({
+    todos,
+    visibleTodos,
+    activeTodosAmount,
+    error,
+    setError,
+    filter,
+    setFilter,
+  }), [
+    visibleTodos,
+    activeTodosAmount,
+    error,
+    filter,
+  ]);
+
+  return (
+    <TodoContext.Provider value={contextValues}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
