8d93c981c31758717ea80e4b6bdb0a9650223205
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-09-22 16:09:26 +0300

    fix mistake

--- src/App.tsx
+++ src/App.tsx
@@ -254,7 +254,7 @@ export const App: React.FC = () => {
 
       {/* Notification is shown in case of any error */}
       {/* Add the 'hidden' class to hide the message smoothly */}
-      {todosError && (
+      {todosError ? (
         <div
           data-cy="ErrorNotification"
           className={`notification is-danger is-light has-text-weight-normal ${todosError ? '' : 'hidden'}`}
@@ -266,7 +266,7 @@ export const App: React.FC = () => {
             onClick={() => setTodosError('')}
           />
           {/* show only one message at a time */}
-          Unable to load todos
+          {todosError}
           <br />
           Title should not be empty
           <br />
@@ -275,6 +275,11 @@ export const App: React.FC = () => {
           Unable to delete a todo
           <br />
           Unable to update a todo
+        </div>
+      ) : (
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
         </div>
       )}
     </div>
