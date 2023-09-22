8ce88f81ae9c9dd6aaa34a7f5f2bae53b45992a7
Daryna Hnidash <melissa998877@gmail.com>
2023-09-22 09:59:32 +0300

    errors improved

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,7 +2,7 @@ import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
 export const getTodos = (userId: number) => {
-  return client.get<Todo[]>(`/todos?userId=${userId}`);
+  return client.get<Todo[]>(`/1todos?userId=${userId}`);
 };
 
 export const deleteTodos = (todoId: number) => {
--- src/types/errorMessage.ts
+++ src/types/errorMessage.ts
@@ -1,6 +1,7 @@
 export enum ErrorMessage {
   NO = '',
-  DOWNLOAD = 'Unable to download todos',
+  DOWNLOAD = 'Unable to load todos',
+  TITLE = 'Title should not be empty',
   ADD = 'Unable to add a todo',
   DELETE = 'Unable to delete a todo',
   UPDATE = 'Unable to update a todo',
