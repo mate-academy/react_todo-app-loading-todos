814624bfdbdee02a012d65d3e0239dad22caa150
Maksym Bevskyi <bevskiymaks@gmail.com>
2023-10-20 17:12:22 +0200

    Fix code

--- src/components/NewTodo/NewTodo.tsx
+++ src/components/NewTodo/NewTodo.tsx
@@ -1,14 +1,12 @@
 type Props = {};
 
-export const NewTodo: React.FC<Props> = () => {
-  return (
-    <form>
-      <input
-        data-cy="NewTodoField"
-        type="text"
-        className="todoapp__new-todo"
-        placeholder="What needs to be done?"
-      />
-    </form>
-  );
-};
+export const NewTodo: React.FC<Props> = () => (
+  <form>
+    <input
+      data-cy="NewTodoField"
+      type="text"
+      className="todoapp__new-todo"
+      placeholder="What needs to be done?"
+    />
+  </form>
+);
