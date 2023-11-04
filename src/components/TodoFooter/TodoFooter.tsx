d7ac5afd0218e9f0adf8e54c70e7b0cb493335ab
Ivanna Zaiets <iva.zaiets@gmail.com>
2023-11-04 12:24:32 +0200

    solution react_todo_app_loading_todos task

---
+++ src/components/TodoFooter/TodoFooter.tsx
@@ -0,0 +1,34 @@
+import { TodoSelecet } from '../TodoSelect/TodoSelect';
+
+type Props = {
+  onTodoSelected: (value: string) => void,
+  isTodoSelected: string,
+  count: number,
+};
+
+export const TodoFooter: React.FC<Props> = ({
+  onTodoSelected,
+  isTodoSelected,
+  count,
+}) => {
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {`${count} items left`}
+      </span>
+
+      <TodoSelecet
+        onTodoSelected={onTodoSelected}
+        isTodoSelected={isTodoSelected}
+      />
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
