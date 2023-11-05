628db7cf2750e181ea1db615e303a6c436c86355
Olena Shchipina <shipina.olena@gmail.com>
2023-11-05 20:15:20 +0200

    solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,8 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
+export const createTodo = (data: Omit<Todo, 'id'>, userId: number) => {
+  return client.post<Todo[]>(`todos?userId=${userId}`, data);
+};
+
 // Add more methods here
