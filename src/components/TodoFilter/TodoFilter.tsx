51f06cb0f426c27d0cb93bbe519db1e2bec63902
Dmytro <tilugdmytro@gmail.com>
2023-11-05 21:19:33 +0200

    split into components, add filtering and loading

---
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import cn from 'classnames';
+import { Filter } from '../../types/Filter';
+
+type Props = {
+  filterBy: Filter;
+  onFilterClick: (value: Filter) => void;
+};
+
+export const TodoFilter: React.FC<Props> = ({ filterBy, onFilterClick }) => {
+  const filterLinks = Object.values(Filter).map((filter) => (
+    <a
+      key={filter}
+      href={`#/${filter.toLowerCase()}`}
+      className={cn('filter__link', { selected: filter === filterBy })}
+      data-cy={`FilterLink${filter}`}
+      onClick={() => onFilterClick(filter)}
+    >
+      {filter}
+    </a>
+  ));
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        3 items left
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        {filterLinks}
+      </nav>
+
+      {/* don't show this button if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
