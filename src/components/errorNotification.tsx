94731c2ed9376c105393cb563925f0466aa2eeb8
Dmytro Panin <dmitriyp1598@gmail.com>
2023-11-06 14:34:08 +0200

    add task solution

---
+++ src/components/errorNotification.tsx
@@ -0,0 +1,35 @@
+import cn from 'classnames';
+import { useState } from 'react';
+
+export const ErrorNotification = () => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  setTimeout(() => setIsHidden(true), 3000);
+
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={cn('notification is-danger is-light has-text-weight-normal', {
+        hidden: isHidden,
+      })}
+    >
+      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setIsHidden(true)}
+      />
+      {/* show only one message at a time */}
+      Unable to load todos
+      <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
---
+++ src/components/todoList.tsx
@@ -0,0 +1,154 @@
+// import React, { useEffect } from 'react';
+// import { getTodos } from '../services/todos';
+import cn from 'classnames';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  // userId: number,
+  // setTodos: (todos: Todo[]) => void,
+  todos: Todo[],
+  // setIsCompletedTodo: (todo: Todo) => void,
+  // isCompletedTodo: number | null,
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  // setIsCompletedTodo,
+}) => (
+  <section className="todoapp__main" data-cy="TodoList">
+    {todos.map(todo => (
+      <div
+        data-cy="Todo"
+        className={cn('todo', {
+          completed: todo.completed,
+        })}
+      >
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+            // onClick={() => setIsCompletedTodo(todo)}
+            checked
+          />
+        </label>
+
+        <span data-cy="TodoTitle" className="todo__title">
+          {todo.title}
+        </span>
+
+        {/* Remove button appears only on hover */}
+        <button type="button" className="todo__remove" data-cy="TodoDelete">
+          ×
+        </button>
+
+        {/* overlay will cover the todo while it is being updated */}
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    ))}
+    {/* This is a completed todo */}
+    <div data-cy="Todo" className="todo completed">
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        Completed Todo
+      </span>
+
+      {/* Remove button appears only on hover */}
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      {/* overlay will cover the todo while it is being updated */}
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+
+    {/* This todo is not completed */}
+    <div data-cy="Todo" className="todo">
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        Not Completed Todo
+      </span>
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+
+    {/* This todo is being edited */}
+    <div data-cy="Todo" className="todo">
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+        />
+      </label>
+
+      {/* This form is shown instead of the title and remove button */}
+      <form>
+        <input
+          data-cy="TodoTitleField"
+          type="text"
+          className="todo__title-field"
+          placeholder="Empty todo will be deleted"
+          value="Todo is being edited now"
+        />
+      </form>
+
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+
+    {/* This todo is in loadind state */}
+    <div data-cy="Todo" className="todo">
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        Todo is being saved now
+      </span>
+
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      {/* 'is-active' class puts this modal on top of the todo */}
+      <div data-cy="TodoLoader" className="modal overlay is-active">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  </section>
+);
