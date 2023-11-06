c4b084d0fdba4d2e97e8a7eb94182b6b3075a812
Oleh Kuper <ok1804@ukr.net>
2023-11-06 22:31:17 +0200

    add solution todo app

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { addTodos } from "../../api/todos";
+import { Todo } from "../../types/Todo";
+import { TodoFromServer } from "../../types/TodoFromServer";
+
+type Props = {
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const todoData: TodoFromServer = {
+  userId: 11853,
+  completed: false,
+  title: '',
+};
+
+export const Header: React.FC<Props> = ({ setTodos }) => {
+  const [titleTodo, setTitleTodo] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    todoData.title = titleTodo;
+
+    addTodos(todoData)
+      .then(data => {
+        return setTodos((currentTodos) => [...currentTodos, data])
+      });
+  }
+
+  return (
+    <header className="todoapp__header">
+      {/* this buttons is active only if there are some active todos */}
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+      />
+
+      {/* Add a todo on form submit */}
+      <form onSubmit={onSubmit}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={titleTodo}
+          onChange={event => setTitleTodo(event.target.value)}
+        />
+      </form>
+    </header>
+  )
+}
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,19 @@
 import { Todo } from '../types/Todo';
+import { TodoFromServer } from '../types/TodoFromServer';
 import { client } from '../utils/fetchClient';
 
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const addTodos = ({userId, completed, title}: TodoFromServer) => {
+  return client.post<Todo>(`/todos`, {userId, completed, title});
+};
+
+export const deleteTodos = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodos = (todoId: number, data: Todo) => {
+  return client.patch<Todo>(`/todos/${todoId}`, data);
+};
