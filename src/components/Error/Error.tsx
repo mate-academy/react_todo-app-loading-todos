9497cde648d38539f1c120ebd0a9696a21ab8469
Oksana Drozdyk <oksanadrozdik66@gmail.com>
2023-11-08 11:44:30 +0200

    another try

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import cn from 'classnames';
+// import cn from 'classnames';
 
 type Props = {
   errorMessage: string;
@@ -19,9 +19,10 @@ export const ErrorMessage: React.FC<Props> = ({
   return (
     <div
       data-cy="ErrorNotification"
-      className={cn('notification is-danger is-light has-text-weight-normal', {
-        hidden: !errorMessage,
-      })}
+      // className={cn('notification is-danger is-light has-text-weight-normal', {
+      //   hidden: !errorMessage,
+      // })}
+      className={`notification is-danger is-light has-text-weight-normal ${!errorMessage && 'hidden'}`}
     >
       {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
       <button
