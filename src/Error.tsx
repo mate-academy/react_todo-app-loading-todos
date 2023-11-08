32b32c2510bb34d9375a3b21379195aac5631722
Anna Shevtsova <aniashevtsova@gmail.com>
2023-11-08 15:45:58 +0100

    all tests pass

---
+++ src/Error.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+
+type Props = {
+  errorMessage: string;
+  setErrorMessage: (newMessage: string) => void;
+};
+
+export const Error: React.FC<Props> = ({
+  errorMessage,
+  setErrorMessage = () => {},
+}) => {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (errorMessage) {
+        setErrorMessage('');
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [errorMessage]);
+
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={`notification is-danger is-light has-text-weight-normal ${!errorMessage && 'hidden'}`}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setErrorMessage('')}
+        aria-label="Close error message"
+      />
+      {errorMessage}
+      {/* show only one message at a time */}
+      {/* Unable to load todos
+      <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo */}
+    </div>
+  );
+};
