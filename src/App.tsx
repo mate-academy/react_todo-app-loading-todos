befdcd037b41bc1e632fc500f546fbf19424f5c2
andrew gidrovich <mr.androvich55555@gmail.com>
2023-11-13 20:23:44 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ export const App: React.FC = () => {
       <div className="todoapp__content">
         <Header />
 
-        {todos.length > 0 && (
+        {todos.length && (
           <List todos={filteredTodos} />
         )}
 
