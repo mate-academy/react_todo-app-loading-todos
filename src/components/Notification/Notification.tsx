f003798c20a1e66498b56772af1e98acdd8e036a
Hanna Anishyna <ann.986@gmail.com>
2023-11-15 12:56:13 -0800

    split into components, add filter and errors functionality

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  error: string
+};
+
+export const Notification: React.FC<Props> = ({ error }) => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  setTimeout(() => {
+    setIsHidden(true)
+  }, 3000);
+
+  return (
+    <>
+      {error && (
+        <div
+          data-cy="ErrorNotification"
+          className={classNames("notification is-danger is-light has-text-weight-normal", {hidden: isHidden})}
+        >
+          <button
+            data-cy="HideErrorButton"
+            type="button"
+            className="delete"
+            onClick={() => setIsHidden(true)}
+          />
+          {error}
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {Todo} from "../../types/Todo";
+import classNames from 'classnames';
+
+type Props = {
+  todo: Todo;
+}
+
+export const TodoItem: React.FC<Props> = ({ todo}) => {
+
+  return (
+    <div>
+      <div data-cy="Todo" className={classNames("todo", {completed: todo.completed})}>
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+            checked={todo.completed}
+          />
+        </label>
+
+        <span data-cy="TodoTitle" className="todo__title">
+          {todo.title}
+        </span>
+
+        {/* Remove button appears only on hover */}
+        <button type="button" className="todo__remove" data-cy="TodoDelete">
+          ×
+        </button>
+
+        {/* overlay will cover the todo while it is being updated */}
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter"/>
+          <div className="loader"/>
+        </div>
+      </div>
+
+        {/*/!* This form is shown instead of the title and remove button *!/*/}
+        {/*<form>*/}
+        {/*  <input*/}
+        {/*    data-cy="TodoTitleField"*/}
+        {/*    type="text"*/}
+        {/*    className="todo__title-field"*/}
+        {/*    placeholder="Empty todo will be deleted"*/}
+        {/*    value="Todo is being edited now"*/}
+        {/*  />*/}
+        {/*</form>*/}
+      </div>
+  );
+};
