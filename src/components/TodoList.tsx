876fd581bccc4905901e1303c24dc6e6fe64af13
Vovchuk <kulivackijvoloda@gmail.com>
2023-11-15 12:17:59 +0200

    add task slution

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,7 +3,7 @@ import { Todo } from '../types/Todo';
 import { TodoItem } from './TodoItem';
 
 type Props = {
-  todos: Todo[] | null;
+  todos: Todo[];
 };
 
 export const TodoList: React.FC<Props> = ({ todos }) => {
