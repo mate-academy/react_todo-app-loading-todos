0e2ac86dea5b66f6366d38908c49311594f6eb28
denys2 <brownexpert@gmail.com>
2023-11-23 20:06:28 +0200

    solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,18 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const addTodo = ({ title, completed, userId }: Omit<Todo, 'id'>) => {
+  return client.post<Todo>('/todos', { title, completed, userId });
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updatedTodo = ({
+  id, title, completed, userId,
+}: Todo) => {
+  const trimmedTitle = title.trim();
+
+  return client.patch<Todo>(`/todos/${id}`, { trimmedTitle, completed, userId });
+};
