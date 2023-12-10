ea05d9fdb5f0c42e90aba2a76f6416c56f8b3dd2
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-11 00:03:27 +0200

    Revert "TODO APP API v1.1"
    This reverts commit 32dcd84cffebf5a93a137aec393ec6bdc7253f44.

--- src/types/Error.ts
+++
@@ -1,7 +0,0 @@
-export enum Error {
-  NONE = '',
-  LOAD = 'Unable to load a todo',
-  ADD = 'Unable to add a todo',
-  DELETE = 'Unable to delete a todo',
-  UPDATE = 'Unable to apdate a todo',
-}
