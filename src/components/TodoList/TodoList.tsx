6db6c8926a73c3e52e69e4911e4df548d4854222
Emiliia Kaminska <milja2705@gmail.com>
2023-12-14 15:09:47 +0200

    added filters

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,42 +1,42 @@
-import { FC } from 'react';
-import { Todo } from '../../types/Todo';
+import {FC} from 'react';
+import {Todo} from '../../types/Todo';
+import cn from 'classnames';
 
 interface Props {
   todos: Todo[],
 }
+
 export const TodoList: FC<Props> = (props) => {
   const {
     todos,
   } = props;
 
-  // console.log(props, 'props');
-
   return (
     <div>
       {todos.map(todo => (
-        <div
-          data-cy="Todo"
-          className="todo"
-          key={todo.id}
-        >
+        <div data-cy="Todo" className={cn('todo', { 'completed': todo.completed })} key={todo.id}>
           <label className="todo__status-label">
             <input
               data-cy="TodoStatus"
               type="checkbox"
               className="todo__status"
+              checked
             />
           </label>
 
           <span data-cy="TodoTitle" className="todo__title">
             {todo.title}
           </span>
+
+          {/* Remove button appears only on hover */}
           <button type="button" className="todo__remove" data-cy="TodoDelete">
             ×
           </button>
 
+          {/* overlay will cover the todo while it is being updated */}
           <div data-cy="TodoLoader" className="modal overlay">
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
+            <div className="modal-background has-background-white-ter"/>
+            <div className="loader"/>
           </div>
         </div>
       ))}
