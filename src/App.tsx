055a89e8d45cb4569afa4c553a9c3dd5fa573002
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2023-12-26 18:26:54 +0100

    display todo in the App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
 import { getTodos } from './api/todos';
 import { Todo } from './types/Todo';
+import { TodoInfo } from './components/TodoInfo';
+import { TodoList } from './components/TodoList';
 
 const USER_ID = 12078;
 
@@ -50,56 +52,9 @@ export const App: React.FC = () => {
         </header>
 
         <section className="todoapp__main" data-cy="TodoList">
-          {/* This is a completed todo */}
-          <div data-cy="Todo" className="todo completed">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-                checked
-              />
-            </label>
-
-            <span data-cy="TodoTitle" className="todo__title">
-              Completed Todo
-            </span>
-
-            {/* Remove button appears only on hover */}
-            <button type="button" className="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
-
-            {/* overlay will cover the todo while it is being updated */}
-            <div data-cy="TodoLoader" className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
-
-          {/* This todo is not completed */}
-          <div data-cy="Todo" className="todo">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-              />
-            </label>
-
-            <span data-cy="TodoTitle" className="todo__title">
-              Not Completed Todo
-            </span>
-            <button type="button" className="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
-
-            <div data-cy="TodoLoader" className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
-
+          <TodoList
+            todos={todos}
+          />
           {/* This todo is being edited */}
           <div data-cy="Todo" className="todo">
             <label className="todo__status-label">
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,20 @@
+import React from 'react';
 import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
 
 type Props = {
   todos: Todo[],
 };
+
+export const TodoList: React.FC<Props> = (
+  { todos },
+) => (
+  <section className="todoapp__main" data-cy="TodoList">
+    {todos
+      .map(todo => (
+        <TodoInfo
+          todo={todo}
+        />
+      ))}
+  </section>
+);
