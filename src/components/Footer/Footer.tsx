d9755b3d901323f86d3471776bc2fa96e9c327eb
Marcin Szymanski <mszymanski1709@outlook.com>
2023-12-27 01:25:05 +0100

    footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,73 @@
+import { FC } from 'react';
+import classnames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todos: Todo[],
+  setFilterValue: (value: string) => void,
+  filterValue: string,
+}
+
+export const Footer: FC<Props> = (props) => {
+  const {
+    todos,
+    setFilterValue,
+    filterValue,
+  } = props;
+
+  const remainingTodosLength = todos.filter(todo => !todo.completed).length;
+  const completedTodosLength = todos.filter(todo => todo.completed).length;
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {`${remainingTodosLength} items left`}
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={classnames('filter__link',
+            { selected: filterValue === 'all' })}
+          data-cy="FilterLinkAll"
+          onClick={() => setFilterValue('all')}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classnames('filter__link',
+            { selected: filterValue === 'active' })}
+          data-cy="FilterLinkActive"
+          onClick={() => setFilterValue('active')}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classnames('filter__link',
+            { selected: filterValue === 'completed' })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => setFilterValue('completed')}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed hidden"
+        data-cy="ClearCompletedButton"
+        style={{
+          visibility: completedTodosLength !== 0
+            ? 'visible' : 'hidden',
+        }}
+      >
+        Clear completed
+      </button>
+
+    </footer>
+  );
+};
