64d8b67a1071de47154a9165291122c117311ac2
Bartek Zmuda <bertsu@protonmail.com>
2023-12-31 12:21:18 +0100

    solution

---
+++ src/Error.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type Props = {
+  errorMessage: string;
+  setErrorMessage: (newMessage: string) => void;
+};
+
+export const Error: React.FC<Props> = ({
+  errorMessage,
+  setErrorMessage = () => {},
+}) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={`notification is-danger is-light has-text-weight-normal ${!errorMessage && 'hidden'}`}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setErrorMessage('')}
+        aria-label="Close error message"
+      />
+      {errorMessage}
+    </div>
+  );
+};
---
+++ src/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Todo } from './types/Todo';
+
+type Props = {
+  todos: Todo[];
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Header: React.FC<Props> = ({ todos }) => {
+  return (
+    <header className="todoapp__header">
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
