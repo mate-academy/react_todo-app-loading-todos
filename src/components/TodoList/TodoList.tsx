13daaf02af37977b4ac085dbe82779cc3f55f507
Zyrak Nur <noorzirak7@gmail.com>
2024-01-02 19:27:44 +0000

    add task solution

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+interface Props {
+  todos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = React.memo(({ todos }) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => <TodoItem key={todo.id} todo={todo} />)}
+    </section>
+  );
+});
