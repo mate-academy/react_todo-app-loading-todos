f44790479af71944f2c17c34ac22d3e47b45982e
Nazar Dudnyk <dudnyk_ak17@nuwm.edu.ua>
2024-01-12 14:47:25 +0200

    add task solution

---
+++ src/components/TodoHed/TodoHed.tsx
@@ -0,0 +1,21 @@
+export const TodoHeader: React.FC = () => {
+  return (
+    <header className="todoapp__header">
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+        aria-label="Toggle All"
+      />
+
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
