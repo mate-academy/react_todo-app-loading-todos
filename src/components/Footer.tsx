d1c4cd1c10d92062a986d17271d1c14b5cc37923
Dmytro Torop <young-z@i.ua>
2024-01-14 10:03:33 +0200

    feat: add loading all, filter and errors

---
+++ src/components/Footer.tsx
@@ -0,0 +1,55 @@
+import { Filter } from '../types/enums/filter';
+
+interface Props {
+  filterTodos: (filter: Filter) => void,
+  isCompletedTodos: boolean,
+}
+
+export const Footer: React.FC<Props> = ({ filterTodos, isCompletedTodos }) => (
+  <footer className="todoapp__footer" data-cy="Footer">
+    <span className="todo-count" data-cy="TodosCounter">
+      3 items left
+    </span>
+
+    {/* Active filter should have a 'selected' class */}
+    <nav className="filter" data-cy="Filter">
+      <a
+        href="#/"
+        className="filter__link selected"
+        data-cy="FilterLinkAll"
+        onClick={() => filterTodos(Filter.All)}
+      >
+        All
+      </a>
+
+      <a
+        href="#/active"
+        className="filter__link"
+        data-cy="FilterLinkActive"
+        onClick={() => filterTodos(Filter.Active)}
+      >
+        Active
+      </a>
+
+      <a
+        href="#/completed"
+        className="filter__link"
+        data-cy="FilterLinkCompleted"
+        onClick={() => filterTodos(Filter.Completed)}
+      >
+        Completed
+      </a>
+    </nav>
+
+    {/* don't show this button if there are no completed todos */}
+    {isCompletedTodos && (
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+      >
+        Clear completed
+      </button>
+    )}
+  </footer>
+);
