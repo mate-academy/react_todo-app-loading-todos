abafbdf942c4fb1119e3cb2af538213b350f50f6
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-01-31 16:33:19 +0200

    done this stage

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,14 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const createTodo = (
+  { title, userId, completed }: Omit<Todo, 'id'>,
+): Promise<Todo> => {
+  return client.post('/todos/', { title, userId, completed });
+};
+
 // Add more methods here
