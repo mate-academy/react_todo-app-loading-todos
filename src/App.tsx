89ef75772ba04ad915511d54699b2bd76f7363a0
Oleksandr Boiko <iccc97@gmail.com>
2024-02-01 20:15:48 +0200

    init cimmit, add `user_id`

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { UserWarning } from './UserWarning';
 
-const USER_ID = 0;
+const USER_ID = 'https://mate.academy/users/109';
 
 export const App: React.FC = () => {
   if (!USER_ID) {
