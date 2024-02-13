aadd5276f67db615728bcd81617e22b7f53b8fcd
mykhalenych <mykhalenych@gmail.com>
2024-02-13 15:19:14 +0200

    feature/prettier added prettier, updated packages

--- src/App.tsx
+++ src/App.tsx
@@ -187,7 +187,6 @@ export const App: React.FC = () => {
         className="notification is-danger is-light has-text-weight-normal"
       >
         <button data-cy="HideErrorButton" type="button" className="delete" />
-
         {/* show only one message at a time */}
         Unable to load todos
         <br />
