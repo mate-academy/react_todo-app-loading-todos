e8d484f33f100ccfc8f8eb574e5fc995a350af64
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-03-10 19:25:08 +0200

    add task  solution

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+type Props = {
+  title: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const Header: React.FC<Props> = ({ title, setTitle }) => {
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  return (
+    <header className="todoapp__header">
+      {/* this button should have `active` class only if all todos are completed */}
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+      />
+
+      {/* Add a todo on form submit */}
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={handleTitleChange}
+        />
+      </form>
+    </header>
+  );
+};
