bcba4417897612c8dd9edb6d141ac5467deca448
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-03-19 16:22:03 +0200

    1st part

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,10 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 296;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+// Add more methods here https://mate.academy/students-api/todos?userId=296
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import classNames from 'classnames';
+import { TodoLoader } from '../TodoLoader/TodoLoader';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoItem: React.FC<Props> = ({ todo }) => {
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames({
+        todo: true,
+        completed: todo.completed,
+      })}
+    >
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      <TodoLoader />
+    </div>
+  );
+};
