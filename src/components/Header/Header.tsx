d6afa99292ac40fb5f51fe3cb631560b746b964c
Ruslan <4irus86@gmail.com>
2024-03-22 19:37:25 +0200

    add solution

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef } from 'react';
+import { Todo } from '../../types/Todo';
+import cn from 'classnames';
+
+type Props = {
+  todos: Todo[];
+  completedTodos: Todo[];
+  activeTodos: Todo[];
+};
+export const Header: React.FC<Props> = ({
+  todos,
+  completedTodos,
+  activeTodos,
+}) => {
+  const addTodoInputRef = useRef<HTMLInputElement>(null);
+  const isClassActive = completedTodos.length > 0 && activeTodos.length === 0;
+
+  useEffect(() => {
+    if (addTodoInputRef.current) {
+      addTodoInputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div>
+      <header className="todoapp__header">
+        {/* this button should have `active` class only if all todos are completed */}
+        {todos.length !== 0 && (
+          <button
+            type="button"
+            className={cn('todoapp__toggle-all', {
+              active: isClassActive,
+            })}
+            data-cy="ToggleAllButton"
+          />
+        )}
+
+        {/* Add a todo on form submit */}
+        <form>
+          <input
+            data-cy="NewTodoField"
+            type="text"
+            className="todoapp__new-todo"
+            placeholder="What needs to be done?"
+            ref={addTodoInputRef}
+          />
+        </form>
+      </header>
+    </div>
+  );
+};
