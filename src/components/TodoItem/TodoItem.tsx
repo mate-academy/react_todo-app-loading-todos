82d0dd624ac77f64878b091826d98788016067fa
Vasya564 <dudlavasil@gmail.com>
2024-03-22 11:45:46 +0200

    Destructure todo in TodoItem

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -6,23 +6,25 @@ type Props = {
 };
 
 export const TodoItem: React.FC<Props> = ({ todo }) => {
+  const { id, title, completed } = todo;
+
   return (
     <div
       data-cy="Todo"
-      className={classNames('todo', { completed: todo.completed })}
-      key={todo.id}
+      className={classNames('todo', { completed: completed })}
+      key={id}
     >
       <label className="todo__status-label" aria-label="todo__status">
         <input
           data-cy="TodoStatus"
           type="checkbox"
           className="todo__status"
-          checked={todo.completed}
+          checked={completed}
         />
       </label>
 
       <span data-cy="TodoTitle" className="todo__title">
-        {todo.title}
+        {title}
       </span>
 
       <button type="button" className="todo__remove" data-cy="TodoDelete">
