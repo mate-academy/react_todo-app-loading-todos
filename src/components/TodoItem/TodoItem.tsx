87b5de9cd970fb5d18798c0e8f94b0e38f4578f8
Viktor Bykh <bykhviktor21@gmail.com>
2024-03-28 18:34:43 +0200

    additionaly fixed linter issue

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -64,9 +64,8 @@ export const TodoItem: React.FC<Props> = ({
       data-cy="Todo"
       className={classNames('todo', { completed: todo.completed })}
     >
-      <label className="todo__status-label" htmlFor="todo-checkbox">
+      <label className="todo__status-label" aria-label="todo__status">
         <input
-          id="todo-checkbox"
           data-cy="TodoStatus"
           type="checkbox"
           className="todo__status"
