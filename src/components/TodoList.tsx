4d8cdd2eadb8dbbe1b06e2100007bec53b534c23
Ivan <tixehack@gmail.com>
2024-03-31 15:18:47 +0300

    add task solution

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+
+interface Props {
+  preparedTodos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ preparedTodos }) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {/* This is a completed todo */}
+      {preparedTodos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
