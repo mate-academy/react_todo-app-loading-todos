80d20f41a4c09147f9d92cdd7a96ddf6653f53ea
Dmitriy Luhasnkiy <luhanskiydmitriy@gmail.com>
2024-04-01 17:45:19 +0300

    update user_id

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,7 +1,7 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 111;
+export const USER_ID = 406;
 
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
