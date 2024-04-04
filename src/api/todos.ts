df9a4449908614e973441cc1e90bf3dcc10fe70f
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-04-04 12:01:30 +0300

    fixed

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -6,9 +6,3 @@ export const USER_ID = 397;
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
-
-// export function createPost({ title, completed, userId }: Omit<Todo, 'id'>) {
-//   return client.<Todo>('/todos', { title, completed, userId });
-// }
-
-// Add more methods here
