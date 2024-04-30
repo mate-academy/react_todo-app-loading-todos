296c8487c4351ab44e81383828df6d726830768e
Maryna Karpova <mari.artarea@gmail.com>
2024-04-30 12:32:12 +0200

    test

---
+++ src/Components/Todo.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+interface Props {
+  id: number;
+  title: string;
+  completed: boolean;
+  onDelete: () => void;
+  onToggle: () => void;
+}
+
+export const Todo: React.FC<Props> = ({
+  id,
+  title,
+  completed,
+  onDelete,
+  onToggle,
+}) => {
+  return (
+    <div data-cy="Todo" data-id={id} className={`todo ${completed ? 'completed' : ''}`}>
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={completed}
+          onChange={onToggle}
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {title}
+      </span>
+
+      <button
+        type="button"
+        className="todo__remove"
+        onClick={onDelete}
+        data-cy="TodoDelete"
+      >
+        ×
+      </button>
+
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,10 @@
-import { Todo } from '../types/Todo';
+import { Todoo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 534;
 
 export const getTodos = () => {
-  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+  return client.get<Todoo[]>(`/todos?userId=${USER_ID}`);
 };
 
 // Add more methods here
