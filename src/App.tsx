202ce657a3868e5dd7f823d1f16f31ae0052b1d2
Yana Momot <yanamomot2004@gmail.com>
2024-05-10 22:52:00 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useRef, useState } from 'react';
 import { UserWarning } from './UserWarning';
 import { USER_ID, getTodos } from './api/todos';
 import { Todo } from './types/Todo';
 import { SortType } from './types/SortType';
+import { Todos } from './components/Todos/Todos';
+import { Footer } from './components/Footer/Footer';
 
 function getFilter(todos: Todo[], sortField: SortType) {
   switch (sortField) {
@@ -19,6 +19,7 @@ function getFilter(todos: Todo[], sortField: SortType) {
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodos, setNewTodos] = useState<Todo[]>([]);
   const [sortField, setSortField] = useState(SortType.All);
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -27,31 +28,31 @@ export const App: React.FC = () => {
   useEffect(() => {
     getTodos()
       .then(setTodos)
-      .catch(() => setErrorMessage('Unable to load todos'));
+      .catch(() => {
+        setErrorMessage('Unable to load todos');
 
-    setTimeout(() => {
-      setErrorMessage('');
-    }, 3000);
+        setTimeout(() => {
+          setErrorMessage('');
+        }, 3000);
+      });
 
     if (activeInput.current) {
       activeInput.current.focus();
     }
   }, []);
 
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  useEffect(
+    () => setNewTodos(() => getFilter(todos, sortField)),
+    [todos, sortField],
+  );
 
-  const sortedTodos = getFilter(todos, sortField);
+  // const sortedTodos = getFilter(todos, sortField);
 
   const everyTodosCompleted = todos.every(todo => todo.completed);
 
-  const oneTodoCompleted = todos.find(todo => todo.completed);
-
-  const activeTodos = todos.reduce(
-    (acc, todo) => (todo.completed ? acc : acc + 1),
-    0,
-  );
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
 
   return (
     <div className="todoapp">
@@ -77,87 +78,14 @@ export const App: React.FC = () => {
           </form>
         </header>
 
-        <section className="todoapp__main" data-cy="TodoList">
-          {sortedTodos.map(todo => (
-            <div
-              data-cy="Todo"
-              className={`todo ${todo.completed && 'completed'}`}
-              key={todo.id}
-            >
-              <label className="todo__status-label">
-                <input
-                  data-cy="TodoStatus"
-                  type="checkbox"
-                  className="todo__status"
-                  checked={todo.completed}
-                />
-              </label>
+        <Todos todos={newTodos} />
 
-              <span data-cy="TodoTitle" className="todo__title">
-                {todo.title}
-              </span>
-
-              <button
-                type="button"
-                className="todo__remove"
-                data-cy="TodoDelete"
-              >
-                ×
-              </button>
-
-              <div data-cy="TodoLoader" className="modal overlay is-active">
-                <div className="modal-background has-background-white-ter" />
-                <div className="loader" />
-              </div>
-            </div>
-          ))}
-        </section>
-
-        {todos.length > 0 && (
-          <footer className="todoapp__footer" data-cy="Footer">
-            <span className="todo-count" data-cy="TodosCounter">
-              {activeTodos} items left
-            </span>
-
-            <nav className="filter" data-cy="Filter">
-              <a
-                href="#/"
-                className={`filter__link ${sortField === SortType.All && 'selected'}`}
-                data-cy="FilterLinkAll"
-                onClick={() => setSortField(SortType.All)}
-              >
-                All
-              </a>
-
-              <a
-                href="#/active"
-                className={`filter__link ${sortField === SortType.Active && 'selected'}`}
-                data-cy="FilterLinkActive"
-                onClick={() => setSortField(SortType.Active)}
-              >
-                Active
-              </a>
-
-              <a
-                href="#/completed"
-                className={`filter__link ${sortField === SortType.Completed && 'selected'}`}
-                data-cy="FilterLinkCompleted"
-                onClick={() => setSortField(SortType.Completed)}
-              >
-                Completed
-              </a>
-            </nav>
-
-            {oneTodoCompleted && (
-              <button
-                type="button"
-                className="todoapp__clear-completed"
-                data-cy="ClearCompletedButton"
-              >
-                Clear completed
-              </button>
-            )}
-          </footer>
+        {todos.length !== 0 && (
+          <Footer
+            todos={todos}
+            sortField={sortField}
+            setSortField={setSortField}
+          />
         )}
       </div>
 
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,7 +1,7 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 611;
+export const USER_ID = 618;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
