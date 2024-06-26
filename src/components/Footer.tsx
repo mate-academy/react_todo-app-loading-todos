88c5e983fa158372e08014255c7af67cc3ff884f
Alina <alinaklimuk04@gmail.com>
2024-06-26 18:53:40 +0300

    add task solution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Status } from '../types/Status';
+import { Todo } from '../types/Todo';
+import cn from 'classnames';
+
+type Props = {
+  todos: Todo[];
+  status: Status;
+  setStatus: (status: Status) => void;
+};
+
+export const Footer: React.FC<Props> = ({ todos, status, setStatus }) => {
+  const todosLeft = todos.filter(todo => !todo.completed).length;
+  const isCompleted = todos.some(todo => todo.completed);
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {`${todosLeft} items left`}
+      </span>
+
+      {/* Active link should have the 'selected' class */}
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={cn('filter__link', {
+            selected: status === Status.All,
+          })}
+          data-cy="FilterLinkAll"
+          onClick={() => setStatus(Status.All)}
+        >
+          {Status.All}
+        </a>
+
+        <a
+          href="#/active"
+          className={cn('filter__link', {
+            selected: status === Status.Active,
+          })}
+          data-cy="FilterLinkActive"
+          onClick={() => setStatus(Status.Active)}
+        >
+          {Status.Active}
+        </a>
+
+        <a
+          href="#/completed"
+          className={cn('filter__link', {
+            selected: status === Status.Completed,
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => setStatus(Status.Completed)}
+        >
+          {Status.Completed}
+        </a>
+      </nav>
+
+      {/* this button should be disabled if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        disabled={!isCompleted}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
