1976b6d87305ca75fd9c635d6c9bbcbbc2155ec2
Mariana Trinko <marjanavv@gmail.com>
2024-06-26 10:46:36 +0200

    check

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,7 @@ export const App: React.FC = () => {
                   className="todo__status"
                   checked={todo.completed}
                   onChange={() => {
-                    handleIsCompleted(todo)
+                    handleIsCompleted(todo);
                   }}
                 />
               </label>
@@ -266,79 +266,79 @@ export const App: React.FC = () => {
 
 
 
-          {/* This todo is an active todo */}
-          {/* <div data-cy="Todo" className="todo">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-              />
-            </label>
+//           {/* This todo is an active todo */}
+//           {/* <div data-cy="Todo" className="todo">
+//             <label className="todo__status-label">
+//               <input
+//                 data-cy="TodoStatus"
+//                 type="checkbox"
+//                 className="todo__status"
+//               />
+//             </label>
 
-            <span data-cy="TodoTitle" className="todo__title">
-              Not Completed Todo
-            </span>
-            <button type="button" className="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
+//             <span data-cy="TodoTitle" className="todo__title">
+//               Not Completed Todo
+//             </span>
+//             <button type="button" className="todo__remove" data-cy="TodoDelete">
+//               ×
+//             </button>
 
-            <div data-cy="TodoLoader" className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div> */}
+//             <div data-cy="TodoLoader" className="modal overlay">
+//               <div className="modal-background has-background-white-ter" />
+//               <div className="loader" />
+//             </div>
+//           </div> */}
 
-          {/* This todo is being edited */}
-          {/* <div data-cy="Todo" className="todo">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-              />
-            </label> */}
+//           {/* This todo is being edited */}
+//           {/* <div data-cy="Todo" className="todo">
+//             <label className="todo__status-label">
+//               <input
+//                 data-cy="TodoStatus"
+//                 type="checkbox"
+//                 className="todo__status"
+//               />
+//             </label> */}
 
-            {/* This form is shown instead of the title and remove button */}
-            {/* <form>
-              <input
-                data-cy="TodoTitleField"
-                type="text"
-                className="todo__title-field"
-                placeholder="Empty todo will be deleted"
-                value="Todo is being edited now"
-              />
-            </form>
+//             {/* This form is shown instead of the title and remove button */}
+//             {/* <form>
+//               <input
+//                 data-cy="TodoTitleField"
+//                 type="text"
+//                 className="todo__title-field"
+//                 placeholder="Empty todo will be deleted"
+//                 value="Todo is being edited now"
+//               />
+//             </form>
 
-            <div data-cy="TodoLoader" className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div> */}
+//             <div data-cy="TodoLoader" className="modal overlay">
+//               <div className="modal-background has-background-white-ter" />
+//               <div className="loader" />
+//             </div>
+//           </div> */}
 
-          {/* This todo is in loadind state */}
-          {/* <div data-cy="Todo" className="todo">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-              />
-            </label>
+//           {/* This todo is in loadind state */}
+//           {/* <div data-cy="Todo" className="todo">
+//             <label className="todo__status-label">
+//               <input
+//                 data-cy="TodoStatus"
+//                 type="checkbox"
+//                 className="todo__status"
+//               />
+//             </label>
 
-            <span data-cy="TodoTitle" className="todo__title">
-              Todo is being saved now
-            </span>
+//             <span data-cy="TodoTitle" className="todo__title">
+//               Todo is being saved now
+//             </span>
 
-            <button type="button" className="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
+//             <button type="button" className="todo__remove" data-cy="TodoDelete">
+//               ×
+//             </button>
 
-            {/* 'is-active' class puts this modal on top of the todo */}
-            {/* <div data-cy="TodoLoader" className="modal overlay is-active">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
+//             {/* 'is-active' class puts this modal on top of the todo */}
+//             {/* <div data-cy="TodoLoader" className="modal overlay is-active">
+//               <div className="modal-background has-background-white-ter" />
+//               <div className="loader" />
+//             </div>
 
-          </div>
-*/}
+//           </div>
+// */}
