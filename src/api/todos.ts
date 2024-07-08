a21d3b3ff73f14b61d14f2100bd91a7482c9fd77
luishenriquetp <luishenriquetp@gmail.com>
2024-07-08 19:30:42 -0300

    Add task solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,9 @@
+// src/api/todos.ts
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 879; // Replace with your user ID
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
-
-// Add more methods here
