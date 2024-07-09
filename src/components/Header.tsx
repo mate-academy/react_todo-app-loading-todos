3f8b9cb45404d87cb6be189bff20bd32ffc6db94
Illia Kocherovets <illiakocherovets@gmail.com>
2024-07-09 16:09:57 +0200

    solution

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import { FC, useState, FormEvent } from 'react';
+
+interface Props {
+  onAdd: (title: string) => void;
+}
+
+export const Header: FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (title.trim()) {
+      onAdd(title.trim());
+      setTitle('');
+    }
+  };
+
+  return (
+    <header className="todoapp__header">
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+      />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </form>
+    </header>
+  );
+};
