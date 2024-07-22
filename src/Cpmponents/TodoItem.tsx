8aa8952c6fed7e1db6dbdf2a91194c3dabc82773
Ivan Vashchuk <vaniavaschuk@gmail.com>
2024-07-22 16:30:24 +0800

    Solution

---
+++ src/Cpmponents/TodoItem.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable */
+import cn from 'classnames';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todo: Todo;
+};
+
+const TodoItem: React.FC<Props> = ({ todo }) => {
+  const { title, completed } = todo;
+
+  return (
+    <div data-cy="Todo" className={cn('todo', { completed })}>
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={completed}
+        />
+      </label>
+      <span data-cy="TodoTitle" className="todo__title">
+        {title}
+      </span>
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      <div data-cy="TodoLoader" className="modal overlay">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
