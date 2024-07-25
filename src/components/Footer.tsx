a796bcb856806bbe319c98a4f8ffc210a8fb5076
Daniil <daniilbarilotti@gmail.com>
2024-07-25 12:35:38 +0200

    add task solution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import cn from 'classnames';
+import { Filter } from '../types/Filter';
+
+interface Props {
+  activeTodosCount: number;
+  areThereCompletedTodos: boolean;
+  filter: Filter;
+  setFilter: (value: Filter) => void;
+}
+
+export const Footer: React.FC<Props> = ({
+  activeTodosCount,
+  areThereCompletedTodos,
+  filter,
+  setFilter,
+}) => (
+  <footer className="todoapp__footer" data-cy="Footer">
+    <span className="todo-count" data-cy="TodosCounter">
+      {activeTodosCount} items left
+    </span>
+
+    <nav className="filter" data-cy="Filter">
+      <a
+        href="#/"
+        className={cn('filter__link', { selected: filter === Filter.all })}
+        data-cy="FilterLinkAll"
+        onClick={() => setFilter(Filter.all)}
+      >
+        All
+      </a>
+
+      <a
+        href="#/"
+        className={cn('filter__link', { selected: filter === Filter.active })}
+        data-cy="FilterLinkActive"
+        onClick={() => setFilter(Filter.active)}
+      >
+        Active
+      </a>
+
+      <a
+        href="#/"
+        className={cn('filter__link', {
+          selected: filter === Filter.completed,
+        })}
+        data-cy="FilterLinkCompleted"
+        onClick={() => setFilter(Filter.completed)}
+      >
+        Completed
+      </a>
+    </nav>
+
+    <button
+      type="button"
+      className="todoapp__clear-completed"
+      data-cy="ClearCompletedButton"
+      disabled={!areThereCompletedTodos}
+    >
+      Clear completed
+    </button>
+  </footer>
+);
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+import { Footer } from './Footer';
+import { Filter } from '../types/Filter';
+
+interface Props {
+  displayedTodos: Todo[];
+  filter: Filter;
+  setFilter: (value: Filter) => void;
+  activeTodosCount: number;
+  areThereCompletedTodos: boolean;
+}
+
+export const TodoList: React.FC<Props> = ({
+  displayedTodos,
+  filter,
+  setFilter,
+  activeTodosCount,
+  areThereCompletedTodos,
+}) => (
+  <>
+    <section className="todoapp__main" data-cy="TodoList">
+      {displayedTodos.map(todo => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </section>
+
+    <Footer
+      activeTodosCount={activeTodosCount}
+      areThereCompletedTodos={areThereCompletedTodos}
+      filter={filter}
+      setFilter={setFilter}
+    />
+  </>
+);
