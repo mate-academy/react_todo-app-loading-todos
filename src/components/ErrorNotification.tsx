e50b0211335d7d36ed782b2529703d7e9bf7cef2
marichkamt <emilycry1999@gmail.com>
2024-08-02 13:52:20 +0200

    added solution

---
+++ src/components/ErrorNotification.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import { Errors } from '../enums/Errors';
+import React from 'react';
+
+interface ErrorNotificationProps {
+  errorMessage: Errors | null;
+  clearErrorMessage: () => void;
+}
+
+export const ErrorNotification: React.FC<ErrorNotificationProps> = ({
+  errorMessage,
+  clearErrorMessage,
+}) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        { hidden: !errorMessage },
+      )}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => clearErrorMessage()}
+      />
+
+      {errorMessage}
+    </div>
+  );
+};
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+
+interface Props {
+  preparedTodos: Todo[];
+}
+
+export const TodoList: React.FC<Props> = ({ preparedTodos }) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {/* This is a completed todo */}
+      {preparedTodos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
