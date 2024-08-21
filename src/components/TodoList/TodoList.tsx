5cc38137dd632e0fac733f06048bd5f0431e91e9
Teollan <serhiikofficial@gmail.com>
2024-08-21 23:36:26 +0300

    initial commit

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,33 @@
+import { TodoCard } from '../TodoCard/TodoCard';
+
+import TodosContext from '../../contexts/Todos/TodosContext';
+import FilterContext from '../../contexts/Filter/FilterContext';
+
+import { Todo } from '../../types';
+import { Filter } from '../../enums';
+
+function getFilteredTodos(todos: Todo[], filter: Filter) {
+  switch (filter) {
+    case Filter.All:
+      return todos;
+    case Filter.Active:
+      return todos.filter(({ completed }) => !completed);
+    case Filter.Completed:
+      return todos.filter(({ completed }) => completed);
+  }
+}
+
+export const TodoList = () => {
+  const { todos } = TodosContext.useState();
+  const { filter } = FilterContext.useState();
+
+  const todosToDisplay = getFilteredTodos(todos, filter);
+
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todosToDisplay.map(todo => (
+        <TodoCard key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
