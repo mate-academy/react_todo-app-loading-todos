0248476d50467427fe0777ec3dda83ab237d7620
AndGrachov <grachovand@gmail.com>
2024-08-22 14:00:17 +0300

    Solution

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,16 @@
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+type Props = {
+  todos: Todo[];
+};
+
+export const TodoList = ({ todos }: Props) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
