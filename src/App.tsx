c8b0d7ece3e2c8bfa37a9490a498337238b1aab0
Vadym Urbanovych <vadymurbanovych@example.com>
2024-08-22 13:15:21 +0200

    solution-lint-fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
 import React, { useEffect, useMemo, useState } from 'react';
 import cn from 'classnames';
 
--- src/components/TodoUser.tsx
+++ src/components/TodoUser.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import { Todo } from '../types/Todo.js';
 import cn from 'classnames';
