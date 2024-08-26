2087e535bbacfa9ef063eacfab1438a810538ecb
Nikita <perfect1201@ukr.net>
2024-08-26 12:58:49 +0300

    divided into components

---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,22 @@
+type Props = {};
+
+export const ErrorNotification: React.FC<Props> = () => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className="notification is-danger is-light has-text-weight-normal"
+    >
+      <button data-cy="HideErrorButton" type="button" className="delete" />
+      {/* show only one message at a time */}
+      Unable to load todos
+      <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
