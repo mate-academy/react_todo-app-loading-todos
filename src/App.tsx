3e4cbde0faaca6fb710e8133ff9baee9f8762a20
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-09-01 15:07:06 +0300

    working with Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
-import { USER_ID } from './api/todos';
+import { getTodos, USER_ID } from './api/todos';
 import Header from './components/Header';
 import TodoList from './components/TodoList';
 import Footer from './components/Footer';
 import Error from './components/Error';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  // Стейт для хранения данных полученных из сервера
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // Стейт для хранения сообщений об ошибке
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  // Получение данных из сервера
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => setTodos(todosFromServer))
+      .catch(() => setErrorMessage('Unable to load todos'));
+  }, []);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
@@ -16,9 +29,9 @@ export const App: React.FC = () => {
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
-
+      {errorMessage}
       <div className="todoapp__content">
-        <Header />
+        <Header todos={todos} />
         <TodoList />
 
         {/* Hide the footer if there are no todos */}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
-const Header = () => {
+import cn from 'classnames';
+import { Todo } from '../types/Todo';
+import React from 'react';
+
+type Props = {
+  todos: Todo[];
+};
+
+const Header: React.FC<Props> = ({ todos }) => {
+  // Проверка, все ли Todos выполнены
+  const allTodosCompleted = todos.every(todo => todo.completed);
+
   return (
     <header className="todoapp__header">
       {/* this button should have `active` class only if all todos are completed */}
-      <button
-        type="button"
-        className="todoapp__toggle-all active"
-        data-cy="ToggleAllButton"
-      />
+      {todos.length > 0 && (
+        <button
+          type="button"
+          className={cn('todoapp__toggle-all', { active: allTodosCompleted })}
+          data-cy="ToggleAllButton"
+        />
+      )}
 
       {/* Add a todo on form submit */}
       <form>
@@ -15,6 +28,7 @@ const Header = () => {
           type="text"
           className="todoapp__new-todo"
           placeholder="What needs to be done?"
+          autoFocus
         />
       </form>
     </header>
