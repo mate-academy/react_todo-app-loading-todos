a8108742d301ba889518dce7cd48420b94deb9ca
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2024-09-16 02:43:15 +0300

    Add enum, add TodoItem component, othr fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,38 @@ import { UserWarning } from './UserWarning';
 import { createTodo, getTodos } from './api/todos';
 import { Todo } from './types/Todo';
 import classNames from 'classnames';
+import { Filter } from './types/Filter';
+import { TodoItem } from './components/TodoItem';
 
 export const USER_ID = 1414;
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [isloading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [title, setTitle] = useState('');
-  const [filter, setFilter] = useState<'all' | 'active' | 'completed'>('all');
+  const [filter, setFilter] = useState<Filter>(Filter.All);
   const [userId] = useState(USER_ID);
   const [completed] = useState(false);
-  const [hasShownError, setHasShownError] = useState(false);
+  //const [hasShownError, setHasShownError] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
+    setIsLoading(true);
     getTodos(userId)
       .then(setTodos)
       .catch(() => {
         setError('Unable to load todos');
         const timeoutId = setTimeout(() => {
-          setError(''); // Clear error after 3 seconds
-          setHasShownError(true); // Mark that the error has been shown
+          setError('');
+          //setHasShownError(true);
         }, 3000);
 
         return () => clearTimeout(timeoutId);
       })
-      .finally(() => setLoading(false));
-  }, [userId, filter, error, hasShownError]);
+      .finally(() => setIsLoading(false));
+  }, [userId]);
 
+  //userId, filter, error, hasShownError
   const reset = () => {
     setTitle('');
     setError('');
@@ -81,16 +84,16 @@ export const App: React.FC = () => {
   const hasCompleted = todos.some(todo => todo.completed);
 
   const filteredTodos = todos.filter(todo => {
-    if (filter === 'active') {
+    if (filter === Filter.Active) {
       return !todo.completed;
-    } else if (filter === 'completed') {
+    } else if (filter === Filter.Completed) {
       return todo.completed;
     }
 
     return true;
   });
 
-  const handleFilterChange = (newFilter: 'all' | 'active' | 'completed') => {
+  const handleFilterChange = (newFilter: Filter) => {
     setFilter(newFilter);
   };
 
@@ -101,7 +104,7 @@ export const App: React.FC = () => {
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
-      {!loading && (
+      {!isloading && (
         <>
           <div className="todoapp__content">
             <header className="todoapp__header">
@@ -134,96 +137,8 @@ export const App: React.FC = () => {
 
             <section className="todoapp__main" data-cy="TodoList">
               {filteredTodos.map(todo => (
-                <div
-                  data-cy="Todo"
-                  key={todo.id}
-                  className={`todo ${todo.completed ? 'completed' : ''}`}
-                >
-                  <label className="todo__status-label">
-                    <input
-                      data-cy="TodoStatus"
-                      type="checkbox"
-                      className="todo__status"
-                      checked={todo.completed}
-                      onChange={() => handleToggle(todo.id)}
-                    />
-                  </label>
-
-                  <span data-cy="TodoTitle" className="todo__title">
-                    {todo.title}
-                  </span>
-
-                  <button
-                    type="button"
-                    className="todo__remove"
-                    data-cy="TodoDelete"
-                  >
-                    ×
-                  </button>
-
-                  {/* overlay will cover the todo while it is being deleted or updated */}
-
-                  <div data-cy="TodoLoader" className="modal overlay">
-                    <div
-                      className="modal-background
-                      has-background-white-ter"
-                    />
-                    <div className="loader" />
-                  </div>
-                </div>
+                <TodoItem todo={todo} key={todo.id} onChange={handleToggle} />
               ))}
-
-              {/* This todo is being edited */}
-              {/* <div data-cy="Todo" className="todo">
-                 <label className="todo__status-label">
-                  <input
-                    data-cy="TodoStatus"
-                    type="checkbox"
-                    className="todo__status"
-                  />
-                </label> */}
-
-              {/* This form is shown instead of the title and remove button */}
-              {/* <form>
-                  <input
-                    data-cy="TodoTitleField"
-                    type="text"
-                    className="todo__title-field"
-                    placeholder="Empty todo will be deleted"
-                    value="Todo is being edited now"
-                  />
-                </form>
-
-                <div data-cy="TodoLoader" className="modal overlay">
-                  <div className="modal-background has-background-white-ter" />
-                  <div className="loader" />
-                </div>
-              </div>*/}
-
-              {/* This todo is in loadind state */}
-              {/* <div data-cy="Todo" className="todo">
-               <label className="todo__status-label">
-                  <input
-                    data-cy="TodoStatus"
-                    type="checkbox"
-                    className="todo__status"
-                  />
-                </label>
-
-                <span data-cy="TodoTitle" className="todo__title">
-                  Todo is being saved now
-                </span>
-
-                <button type="button" className="todo__remove" data-cy="TodoDelete">
-                  ×
-                </button> */}
-
-              {/* 'is-active' class puts this modal on top of the todo */}
-              {/* <div data-cy="TodoLoader" className="modal overlay is-active">
-                  <div className="modal-background has-background-white-ter" />
-                  <div className="loader" />
-                </div> */}
-              {/* </div> */}
             </section>
 
             {todos.length > 0 && (
@@ -233,38 +148,20 @@ export const App: React.FC = () => {
                 </span>
 
                 <nav className="filter" data-cy="Filter">
-                  <a
-                    href="#/"
-                    className={classNames('filter__link', {
-                      selected: filter === 'all',
-                    })}
-                    data-cy="FilterLinkAll"
-                    onClick={() => handleFilterChange('all')}
-                  >
-                    All
-                  </a>
-
-                  <a
-                    href="#/active"
-                    className={classNames('filter__link', {
-                      selected: filter === 'active',
-                    })}
-                    data-cy="FilterLinkActive"
-                    onClick={() => handleFilterChange('active')}
-                  >
-                    Active
-                  </a>
-
-                  <a
-                    href="#/completed"
-                    className={classNames('filter__link', {
-                      selected: filter === 'completed',
-                    })}
-                    data-cy="FilterLinkCompleted"
-                    onClick={() => handleFilterChange('completed')}
-                  >
-                    Completed
-                  </a>
+                  {Object.values(Filter).map(filterValue => (
+                    <a
+                      key={filterValue}
+                      href={`#/${filterValue}`}
+                      className={classNames('filter__link', {
+                        selected: filter === filterValue,
+                      })}
+                      data-cy={`FilterLink${filterValue.charAt(0).toUpperCase() + filterValue.slice(1)}`}
+                      onClick={() => handleFilterChange(filterValue)}
+                    >
+                      {filterValue.charAt(0).toUpperCase() +
+                        filterValue.slice(1)}
+                    </a>
+                  ))}
                 </nav>
 
                 <button
@@ -290,10 +187,6 @@ export const App: React.FC = () => {
       >
         <button data-cy="HideErrorButton" type="button" className="delete" />
         <div>{error}</div>
-        {/*
-        Unable to delete a todo
-        <br />
-        Unable to update a todo */}
       </div>
     </div>
   );
