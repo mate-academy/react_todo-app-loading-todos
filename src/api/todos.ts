d5b46131a34cfcc5371b017c9f049349e0a1db83
Vasyl Zhyliakov <snak87@gmail.com>
2024-10-06 19:33:13 +0300

    part1

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,13 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 1549;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
+export const createTodo = ({ userId, title, completed }: Omit<Todo, 'id'>) => {
+  return client.post<Todo>('/todos', { userId, title, completed });
+};
 // Add more methods here
---
+++ src/components/todoList.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { Todo } from '../types/Todo';
+
+type Props = {
+  visibleTodos: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ visibleTodos }) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {visibleTodos.map(todo => (
+        <div
+          data-cy="Todo"
+          className={cn("todo", {"completed": todo.completed})} 
+          key={todo.id}
+        >
+          <label className="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              className="todo__status"
+              checked
+            />
+          </label>
+
+          <span data-cy="TodoTitle" className="todo__title">
+            {todo.title}
+          </span>
+
+          {/* Remove button appears only on hover */}
+          <button type="button" className="todo__remove" data-cy="TodoDelete">
+            ×
+          </button>
+
+          {/* overlay will cover the todo while it is being deleted or updated */}
+          <div data-cy="TodoLoader" className="modal overlay">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
