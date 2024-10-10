ead9cd105a58735b7f15791eee29e56efad1f754
Rostyslav <rostislavkpi@gmail.com>
2024-10-10 17:53:52 +0300

    Solution

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem';
+
+interface Props {
+  todos: Todo[];
+}
+
+export const TodoList: FC<Props> = ({ todos }) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
---
+++ src/types/Errors.ts
@@ -0,0 +1,7 @@
+export enum Errors {
+  LOAD_ERROR = 'Unable to load todos',
+  TITLE_ERROR = 'Title should not be empty',
+  ADD_TODO = 'Unable to add a todo',
+  DELETE_TODO = 'Unable to delete a todo',
+  UPDATE_TODO = 'Unable to update a todo',
+}
