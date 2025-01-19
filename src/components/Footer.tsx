248f3a44e4cd4bb64ac93f6747d31a7b6eb773b2
Artem Zhyrnyi <ic3ego@gmail.com>
2025-01-19 20:31:19 +0200

    solution

---
+++ src/components/Footer.tsx
@@ -0,0 +1,70 @@
+import cn from 'classnames';
+
+import { Todo } from '../types/Todo';
+import { FilterOptions } from '../types/FilterOptions';
+
+type Props = {
+  todos: Todo[];
+  filterOption: FilterOptions;
+  onFilter: (newFilterOption: FilterOptions) => void;
+};
+
+export default function Footer({ todos, filterOption, onFilter }: Props) {
+  const hasLeftTodos = todos.filter(todo => !todo.completed).length;
+  const hasCompletedTodos = todos.filter(todo => todo.completed);
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {hasLeftTodos} items left
+      </span>
+
+      {/* Active link should have the 'selected' class */}
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={cn('filter__link', {
+            selected: filterOption === FilterOptions.ALL,
+          })}
+          data-cy="FilterLinkAll"
+          onClick={() => onFilter(FilterOptions.ALL)}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={cn('filter__link', {
+            selected: filterOption === FilterOptions.ACTIVE,
+          })}
+          data-cy="FilterLinkActive"
+          onClick={() => onFilter(FilterOptions.ACTIVE)}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={cn('filter__link', {
+            selected: filterOption === FilterOptions.COMPLETED,
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => onFilter(FilterOptions.COMPLETED)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {/* this button should be disabled if there are no completed todos */}
+      {hasCompletedTodos.length > 0 && (
+        <button
+          type="button"
+          className="todoapp__clear-completed"
+          data-cy="ClearCompletedButton"
+        >
+          Clear completed
+        </button>
+      )}
+    </footer>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import cn from 'classnames';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[];
+};
+
+export default function Header({ todos }: Props) {
+  const hasAllTodosCompleted = todos.every(todo => todo.completed);
+
+  return (
+    <header className="todoapp__header">
+      {/* this button should have `active` class only if all todos are completed */}
+      {todos.length > 0 && (
+        <button
+          type="button"
+          className={cn('todoapp__toggle-all', {
+            active: hasAllTodosCompleted,
+          })}
+          data-cy="ToggleAllButton"
+        />
+      )}
+
+      {/* Add a todo on form submit */}
+      <form>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+        />
+      </form>
+    </header>
+  );
+}
