96fd57076b4414b38094ed5f23254f952c4a3199
Anna <annanicky90gmail.com>
2025-01-20 23:12:05 +0400

    implement a simple Todo App

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,12 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 2273;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+export const createTodo = (todo: Todo) => {
+  return client.patch<Todo>(`/todos/${todo.id}`, todo);
+};
