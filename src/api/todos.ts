49cf1b4c847beb389761f8eea9d3180ebc229743
Yaroslav65 <yarikelectro@gmail.com>
2025-01-25 05:31:22 +0100

    react_todo-app-loading-todos

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,14 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 2267;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
+
+// export const updateTodos = ({ id, userId, title, completed }: Todo) => {
+//   return client.patch<Todo[]>(`/todos/${id}`, { userId, title, completed });
+// };
 
 // Add more methods here
