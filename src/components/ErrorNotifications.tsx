bc3de39c0468f5fb7d27146b2cc0ab9c30a7c3e1
D0dgerJ <d0dgerj89@gmail.com>
2025-01-29 16:14:54 +0200

    solution

---
+++ src/components/ErrorNotifications.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+
+type Props = {
+  errorMessage: string;
+  setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const ErrorNotifications: React.FC<Props> = ({
+  errorMessage,
+  setErrorMessage,
+}) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification',
+        'is-danger',
+        'is-light',
+        'has-text-weight-normal',
+        { hidden: !errorMessage },
+      )}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => {
+          setErrorMessage('');
+        }}
+      />
+      {errorMessage}
+    </div>
+  );
+};
