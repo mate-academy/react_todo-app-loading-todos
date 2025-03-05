5b27d095f9f5f5b1debae26ec417072785386e40
ArtemHryb <artem.mushroom.gh@gmail.com>
2025-03-05 13:58:47 +0200

    add solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,14 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 2382;
 
-export const getTodos = () => {
-  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
-};
+export const getTodos = () => client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 
-// Add more methods here
+export const addTodo = (title: string) =>
+  client.post<Todo>('/todos', { userId: USER_ID, title, completed: false });
+
+export const updateTodo = (todoId: number, data: Partial<Todo>) =>
+  client.patch<Todo>(`/todos/${todoId}`, data);
+
+export const deleteTodo = (todoId: number) => client.delete(`/todos/${todoId}`);
