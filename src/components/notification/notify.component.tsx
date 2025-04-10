fceaf5e3441671e5f606d1ed4ae6493d0379493f
Radymyr <loboda.radymyr@gmail.com>
2025-04-10 16:53:15 +0300

    Hide the list and the footer if there are no todos yet

--- src/components/notification/notify.component.tsx
+++ src/components/notification/notify.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classNames from 'classnames';
-import { NotificationTypes } from "./notification.types";
+import { NotificationTypes } from './notification.types';
 
 export const NotifyComponent: React.FC<NotificationTypes> = ({
   closeModal,
