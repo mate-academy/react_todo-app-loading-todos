42f4d7012af3a7049322ac6dd2f2f800baad108e
Евгений Котвицкий <ekotvizky@gmail.com>
2025-04-25 16:14:09 +0300

    devide app to components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,52 @@
+interface FooterProps {
+  itemsLeft: number;
+}
+
+export const Footer: React.FC<FooterProps> = ({ itemsLeft }) => {
+  return (
+    <>
+      {/* Hide the footer if there are no todos */}
+      <footer className="todoapp__footer" data-cy="Footer">
+        <span className="todo-count" data-cy="TodosCounter">
+          {itemsLeft} items left
+        </span>
+
+        {/* Active link should have the 'selected' class */}
+        <nav className="filter" data-cy="Filter">
+          <a
+            href="#/"
+            className="filter__link selected"
+            data-cy="FilterLinkAll"
+          >
+            All
+          </a>
+
+          <a
+            href="#/active"
+            className="filter__link"
+            data-cy="FilterLinkActive"
+          >
+            Active
+          </a>
+
+          <a
+            href="#/completed"
+            className="filter__link"
+            data-cy="FilterLinkCompleted"
+          >
+            Completed
+          </a>
+        </nav>
+
+        {/* this button should be disabled if there are no completed todos */}
+        <button
+          type="button"
+          className="todoapp__clear-completed"
+          data-cy="ClearCompletedButton"
+        >
+          Clear completed
+        </button>
+      </footer>
+    </>
+  );
+};
---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,28 @@
+import cn from 'classnames';
+
+interface NotificationProps {
+  errorMessage: string;
+}
+
+export const Notification: React.FC<NotificationProps> = ({ errorMessage }) => {
+  return (
+    <>
+      {/* DON'T use conditional rendering to hide the notification */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+      <div
+        data-cy="ErrorNotification"
+        className={cn(
+          'notification',
+          'is-danger',
+          'is-light',
+          'has-text-weight-normal',
+          { hidden: !errorMessage },
+        )}
+      >
+        <button data-cy="HideErrorButton" type="button" className="delete" />
+        {/* show only one message at a time */}
+        {errorMessage}
+      </div>
+    </>
+  );
+};
