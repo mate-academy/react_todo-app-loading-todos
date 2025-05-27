687666f8023e96c130dba1131aa3efc02881b342
João Andreassa <joaovra@gmail.com>
2025-05-27 17:22:46 -0300

    solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,10 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 1433;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+export type { Todo };
