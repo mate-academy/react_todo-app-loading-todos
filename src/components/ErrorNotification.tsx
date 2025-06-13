d46a2c78b2e2a899573206af240aa81e5177c584
NataliaDavida <nataliadavida88@gmail.com>
2025-06-13 18:50:20 +0300

    add solution

---
+++ src/components/ErrorNotification.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+
+{
+  /* DON'T use conditional rendering to hide the notification */
+}
+
+{
+  /* Add the 'hidden' class to hide the message smoothly */
+}
+
+type Props = {
+  error: string;
+  setError: (message: string) => void;
+};
+
+export const ErrorNotification: React.FC<Props> = ({ error, setError }) => (
+  <div
+    data-cy="ErrorNotification"
+    className={classNames(
+      'notification is-danger is-light has-text-weight-normal',
+      { hidden: !error },
+    )}
+  >
+    <button
+      data-cy="HideErrorButton"
+      type="button"
+      className="delete"
+      onClick={() => setError('')}
+    />
+    {/* show only one message at a time */}
+    {/* Unable to load todos
+    <br />
+    Title should not be empty
+    <br />
+    Unable to add a todo
+    <br />
+    Unable to delete a todo
+    <br />
+    Unable to update a todo */}
+    {error}
+  </div>
+);
---
+++ src/components/Footer.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import classNames from 'classnames';
+import { FilterType } from '../App';
+
+type Props = {
+  filter: FilterType;
+  setFilter: (filter: FilterType) => void;
+  countActiveTodos: number;
+};
+
+export const Footer: React.FC<Props> = ({
+  filter,
+  setFilter,
+  countActiveTodos,
+}) => {
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {countActiveTodos} left
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={classNames('filter__link', {
+            selected: filter === FilterType.All,
+          })}
+          data-cy="FilterLinkAll"
+          onClick={() => setFilter(FilterType.All)}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames('filter__link', {
+            selected: filter === FilterType.Active,
+          })}
+          data-cy="FilterLinkActive"
+          onClick={() => setFilter(FilterType.Active)}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames('filter__link', {
+            selected: filter === FilterType.Completed,
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => setFilter(FilterType.Completed)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        // onClick={handleClearCompleted}
+        // disabled={!hasCompleted}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
