3b7483889995fc2e2623a52917f2170e501580ec
Yura Soroka <ykotov18@gmail.com>
2025-06-16 09:05:17 +0300

    sol

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,7 +1,19 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3095;
+
+export const TodoServiceErrors = {
+  Unknown: 'Something when wrong with todos',
+  UnableToLoad: 'Unable to load todos',
+  TitleShouldNotBeEmpty: 'Title should not be empty',
+  UnableToAdd: 'Unable to add a todo',
+  UnableToDelete: 'Unable to delete a todo',
+  UnableToUpdate: 'Unable to update a todo',
+} as const;
+
+export type TodoError =
+  (typeof TodoServiceErrors)[keyof typeof TodoServiceErrors];
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
