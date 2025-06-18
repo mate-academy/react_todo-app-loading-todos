7113f5260710ada2a2ffe471e40803a6798bfa6d
Kushnirenko <andrey.2024345@gmail.com>
2025-06-18 11:44:41 +0300

    Solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,19 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3151;
 
+export const TodoServiseErrors = {
+  Unknown: 'Something when wrong with yodos',
+  UnableToLoad: 'Unable to load todos',
+  TitleShouldNotBeEmpty: 'Title should not be empty',
+  UnableToAddTodo: 'Unable to add a todo',
+  UnableToDeleteTodo: 'Unable to delete a todo',
+  UnableToUpdateTodo: 'Unable to update a todo',
+} as const;
+
+export type TodoError =
+  (typeof TodoServiseErrors)[keyof typeof TodoServiseErrors];
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
