433e1255d8e06de9b7b3376ca2fe552261d2d7f0
Arber <1234arberzylyftari1234@gmail.com>
2025-08-14 16:57:03 +0200

    Solution??

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,79 @@
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { FilterStatus } from '../../types/FilterStatus';
+
+type Props = {
+  todos: Todo[];
+  filterStatus: FilterStatus;
+  setFilterStatus: (filterStatus: FilterStatus) => void;
+};
+
+export const Filter: React.FC<Props> = ({
+  todos,
+  filterStatus,
+  setFilterStatus,
+}) => {
+  const completedTodosCount = todos.filter(todo => todo.completed).length;
+  const activeTodosCount = todos.length - completedTodosCount;
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {activeTodosCount} {activeTodosCount === 1 ? 'item' : 'items'} left
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={classNames('filter__link', {
+            selected: filterStatus === 'all',
+          })}
+          data-cy="FilterLinkAll"
+          onClick={e => {
+            e.preventDefault();
+            setFilterStatus('all');
+          }}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames('filter__link', {
+            selected: filterStatus === 'active',
+          })}
+          data-cy="FilterLinkActive"
+          onClick={e => {
+            e.preventDefault();
+            setFilterStatus('active');
+          }}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames('filter__link', {
+            selected: filterStatus === 'completed',
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={e => {
+            e.preventDefault();
+            setFilterStatus('completed');
+          }}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        disabled={completedTodosCount === 0}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[];
+  inputRef: React.RefObject<HTMLInputElement>;
+};
+
+export const Header: React.FC<Props> = ({ todos, inputRef }) => {
+  const areAllCompleted =
+    todos.length > 0 && todos.every(todo => todo.completed);
+
+  return (
+    <header className="todoapp__header">
+      {/* this button should have `active` class only if all todos are completed */}
+      <button
+        type="button"
+        className={classNames('todoapp__toggle-all', {
+          active: areAllCompleted,
+        })}
+        data-cy="ToggleAllButton"
+      />
+
+      <form>
+        <input
+          ref={inputRef}
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+        />
+      </form>
+    </header>
+  );
+};
