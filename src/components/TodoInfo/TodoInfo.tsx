d3ac8ba58eb9084e376e62082ae579ab353b0d8f
Mykyta Musaiev <n.musaiev@gmail.com>
2025-08-21 19:12:28 +0300

    pt.1 done

---
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  const handleTodoStatusChange = (
+    e: React.MouseEvent<HTMLLabelElement, MouseEvent>,
+  ) => {
+    console.log(e.currentTarget);
+  };
+
+  return (
+    <>
+      <div data-cy="Todo" className={cn('todo', { completed: todo.completed })}>
+        <label className="todo__status-label" onClick={handleTodoStatusChange}>
+          <input
+            id={`todo-title-${todo.id}`}
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+            checked={todo.completed}
+          />
+        </label>
+        <span data-cy="TodoTitle" className="todo__title">
+          {todo.title}
+        </span>
+        {/* Remove button appears only on hover */}
+        <button type="button" className="todo__remove" data-cy="TodoDelete">
+          ×
+        </button>
+        {/* overlay will cover the todo while it is being deleted or updated */}
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TodoInfo;
