80a091ee6a5e6a587386b122cb822a86f33a6f79
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-08-25 09:48:08 +0200

    add a task solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,14 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3289;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
 // Add more methods here
+
+export const updateTodoStatus = (id: number, completed: boolean) => {
+  return client.patch<Todo>(`/todos/${id}`, { completed });
+};
