47a58106cd002e5c31926136a4e6a05d5eadf5e6
Dima Ilchenko <dima.ilchenko.ol@gmail.com>
2025-08-28 13:46:38 +0300

    fix1

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,24 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 1;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+export const addTodo = (title: string) => {
+  return client.post<Todo>('/todos', {
+    title,
+    userId: USER_ID,
+    completed: false,
+  });
+};
+
+export const updateTodo = (id: number, data: Todo) => {
+  return client.patch<Todo>(`/todos/${id}`, data);
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
