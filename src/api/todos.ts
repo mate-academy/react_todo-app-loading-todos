a8005c47eb3a26a4f9057a0d027010bee0d639da
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-10-13 21:48:48 +0300

    fix lint error

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -4,20 +4,5 @@ import { client } from '../utils/fetchClient';
 export const USER_ID = 3547;
 
 export const getTodos = (userId: number) => {
-  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
-};
-
-export const createTodo = (title: string) => {
-  return client.post<Todo>('/todos', {
-    title,
-    completed: false,
-  });
-};
-
-export const updateTodo = (id: number, updates: Partial<Todo>) => {
-  return client.patch<Todo>(`/todos/${id}`, updates);
-};
-
-export const deleteTodo = (id: number) => {
-  return client.delete<void>(`/todos/${id}`);
+  return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
