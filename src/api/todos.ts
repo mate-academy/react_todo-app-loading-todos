2c11cbda9c42225570b8f6344be440e18f3c82ff
Jeny Berezhna <JenySmails24@gmail.com>
2025-11-01 17:54:02 +0000

    add task solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -7,4 +7,30 @@ export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+export const createTodo = (title: string) => {
+  return client.post<Todo>('/todos', {
+    userId: USER_ID,
+    title,
+    completed: false,
+  });
+};
+
+export const updateTodo = (todoId: number, data: Partial<Todo>) => {
+  return client.patch<Todo>(`/todos/${todoId}`, data);
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const toggleAllTodos = (todos: Todo[], completed: boolean) => {
+  return Promise.all(
+    todos.map(todo => client.patch<Todo>(`/todos/${todo.id}`, { completed })),
+  );
+};
+
+export const clearCompletedTodos = (todos: Todo[]) => {
+  const completed = todos.filter(todo => todo.completed);
+
+  return Promise.all(completed.map(todo => client.delete(`/todos/${todo.id}`)));
+};
