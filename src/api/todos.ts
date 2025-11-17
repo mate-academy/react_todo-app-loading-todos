d45f42ac9c2df842141eda2826cc759e8c644b0b
Uandrik Laptop <uandriy4141@gmail.com>
2025-11-18 01:58:55 +1400

    develop

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,14 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3688;
 
-export const getTodos = () => {
-  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+export const getTodos = (userId: number) => {
+  return client.get<Todo[]>(`/todos?userId=${userId}`);
+};
+
+export const postTodo = ({ title, userId, completed }: Omit<Todo, 'id'>) => {
+  return client.post<Todo>('/todos', { title, userId, completed });
 };
 
 // Add more methods here
---
+++ src/types/Errors.ts
@@ -0,0 +1,3 @@
+export enum Errors {
+  Loading = 'Unable to load todos',
+}
