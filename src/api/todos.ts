62ae5161f470c5a026189f447e4510db4d9a4b96
douae <studenttec8gmail.com>
2025-12-22 00:30:46 +0100

    done

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,17 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 37;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
+export const createTodo = (todo: Omit<Todo, 'id'>) => {
+  return client.post<Todo>('/todos', todo);
+};
+
+export const removeTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
 // Add more methods here
