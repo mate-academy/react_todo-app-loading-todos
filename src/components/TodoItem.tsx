3870400cec375db4c6c9fc2ecc2fa0678a0925fd
Ivan Shcherbatiuk <shcherbatyuk13@gmail.com>
2026-01-07 01:14:15 +0200

    add task solution

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import classNames from 'classnames';
+
+type Props = {
+  todo: Todo;
+  loading: boolean;
+};
+
+export const TodoItem: React.FC<Props> = ({ todo, loading }) => {
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames('todo', { completed: todo.completed })}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          readOnly
+          aria-label="Toggle todo status"
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      <div
+        data-cy="TodoLoader"
+        className={classNames('modal overlay', { 'is-active': loading })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
