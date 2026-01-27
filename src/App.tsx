9272a437164aca27b84c5aaf4a8690cb3a10477a
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-01-27 18:05:16 +0200

    added first Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
-import { USER_ID } from './api/todos';
+import React, { useState, useEffect } from 'react';
+import { Todo } from './types/Todo';
+import cn from 'classnames';
+
+// import { UserWarning } from './UserWarning';
+// import { USER_ID } from './api/todos';
+
+import * as todoService from './api/todos';
+
+export type FilterStatus = 'All' | 'Active' | 'Completed';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setloading] = useState<boolean>();
+  const [error, setError] = useState<string>();
+  const [filter, setFilter] = useState<FilterStatus>('All');
+
+  const haveTodos = todos.length > 0;
+  const activeTodosCount = todos.filter(t => !t.completed).length;
+
+  function handleError(message: string) {
+    setError(message);
+
+    setTimeout(() => {
+      setError('');
+    }, 3000);
   }
+
+  function handleStatusSwitch(todoId: number, todoStatus: boolean) {}
+
+  function handleErrorReset() {
+    setError('');
+  }
+
+  useEffect(() => {
+    setError('');
+    setloading(true);
+    todoService
+      .getTodos()
+      .then(setTodos)
+      .catch(() => handleError('Unable to load todos'))
+      .finally(() => setloading(false));
+  }, []);
+
+  const visibleTodos = todos.filter(todo => {
+    const matchesStatus =
+      filter === 'All' ||
+      (filter === 'Completed' && todo.completed) ||
+      (filter === 'Active' && !todo.completed);
+
+    return matchesStatus;
+  });
 
   return (
     <div className="todoapp">
@@ -34,6 +78,45 @@ export const App: React.FC = () => {
         </header>
 
         <section className="todoapp__main" data-cy="TodoList">
+          {visibleTodos.map(todo => {
+            return (
+              <div
+                data-cy="Todo"
+                className={cn('todo', { completed: todo.completed })}
+                key={todo.id}
+              >
+                <label className="todo__status-label">
+                  <input
+                    data-cy="TodoStatus"
+                    type="checkbox"
+                    className="todo__status"
+                    checked={todo.completed}
+                    onClick={() => handleStatusSwitch(todo.id, todo.completed)}
+                  />
+                </label>
+
+                <span data-cy="TodoTitle" className="todo__title">
+                  {todo.title}
+                </span>
+
+                {/* Remove button appears only on hover */}
+                <button
+                  type="button"
+                  className="todo__remove"
+                  data-cy="TodoDelete"
+                >
+                  ×
+                </button>
+
+                {/* overlay will cover the todo while it is being deleted or updated */}
+                <div data-cy="TodoLoader" className="modal overlay">
+                  <div className="modal-background has-background-white-ter" />
+                  <div className="loader" />
+                </div>
+              </div>
+            );
+          })}
+
           {/* This is a completed todo */}
           <div data-cy="Todo" className="todo completed">
             <label className="todo__status-label">
@@ -137,17 +220,19 @@ export const App: React.FC = () => {
           </div>
         </section>
 
-        {/* Hide the footer if there are no todos */}
-        <footer className="todoapp__footer" data-cy="Footer">
+        <footer
+          className={cn('todoapp__footer', { hidden: !haveTodos })}
+          data-cy="Footer"
+        >
           <span className="todo-count" data-cy="TodosCounter">
-            3 items left
+            {activeTodosCount} items left
           </span>
 
-          {/* Active link should have the 'selected' class */}
           <nav className="filter" data-cy="Filter">
             <a
               href="#/"
-              className="filter__link selected"
+              className={cn('filter__link', { selected: filter === 'All' })}
+              onClick={() => setFilter('All')}
               data-cy="FilterLinkAll"
             >
               All
@@ -155,7 +240,8 @@ export const App: React.FC = () => {
 
             <a
               href="#/active"
-              className="filter__link"
+              className={cn('filter__link', { selected: filter === 'Active' })}
+              onClick={() => setFilter('Active')}
               data-cy="FilterLinkActive"
             >
               Active
@@ -163,7 +249,10 @@ export const App: React.FC = () => {
 
             <a
               href="#/completed"
-              className="filter__link"
+              className={cn('filter__link', {
+                selected: filter === 'Completed',
+              })}
+              onClick={() => setFilter('Completed')}
               data-cy="FilterLinkCompleted"
             >
               Completed
@@ -181,23 +270,21 @@ export const App: React.FC = () => {
         </footer>
       </div>
 
-      {/* DON'T use conditional rendering to hide the notification */}
-      {/* Add the 'hidden' class to hide the message smoothly */}
       <div
         data-cy="ErrorNotification"
-        className="notification is-danger is-light has-text-weight-normal"
+        className={cn(
+          'notification is-danger is-light has-text-weight-normal',
+          { hidden: !error },
+        )}
       >
-        <button data-cy="HideErrorButton" type="button" className="delete" />
-        {/* show only one message at a time */}
-        Unable to load todos
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={() => handleErrorReset()}
+        />
+        {error}
         <br />
-        Title should not be empty
-        <br />
-        Unable to add a todo
-        <br />
-        Unable to delete a todo
-        <br />
-        Unable to update a todo
       </div>
     </div>
   );
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,24 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3893;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+// export const createTodo = (data) => {
+//   return client.post<Todo>(`/todos?userId=${USER_ID}`);
+// };
+
+// export const updateTodo = (todoId, data) => {
+//   return client.patch<Todo>(`/todos?userId=${USER_ID}`);
+// };
+
+//
+// My User Id = 3893 DON'T DELETE
+//https://mate.academy/students-api/todos?userId=3893
