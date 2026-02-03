c8d1c4aff45bfc4e16a3531d9164005d72edae3a
Matheus <theus.sousaa@gmail.com>
2026-02-03 10:42:40 -0300

    finished project

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,18 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3929;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+export type NewTodo = Omit<Todo, 'id'>;
+
+export const postTodos = (newTodo: NewTodo) => {
+  return client.post<Todo>(`/todos?userId=${USER_ID}`, newTodo);
+};
+
+export const deleteTodos = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
