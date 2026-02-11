4dff70a735b05cfbb604ab9e74eb642e73f3d20c
Dmytro Popov <lort20178@gmail.com>
2026-02-11 11:26:51 +0200

    Add solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,10 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3969;
 
-export const getTodos = () => {
+export const getTodos = (): Promise<Todo[]> => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+// Add more methods here (Add, Delete, Update) for next parts
--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -8,25 +8,23 @@ function wait(delay: number) {
   });
 }
 
-// To have autocompletion and avoid mistypes
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
-function request<T>(
+function request<T, D = unknown>(
   url: string,
   method: RequestMethod = 'GET',
-  data: any = null, // we can send any data to the server
+  data?: D,
 ): Promise<T> {
   const options: RequestInit = { method };
 
   if (data) {
-    // We add body and Content-Type only for the requests with data
     options.body = JSON.stringify(data);
     options.headers = {
       'Content-Type': 'application/json; charset=UTF-8',
     };
   }
 
-  // DON'T change the delay it is required for tests
+  // delay 100ms required for tests
   return wait(100)
     .then(() => fetch(BASE_URL + url, options))
     .then(response => {
@@ -34,13 +32,13 @@ function request<T>(
         throw new Error();
       }
 
-      return response.json();
+      return response.json() as Promise<T>;
     });
 }
 
 export const client = {
   get: <T>(url: string) => request<T>(url),
-  post: <T>(url: string, data: any) => request<T>(url, 'POST', data),
-  patch: <T>(url: string, data: any) => request<T>(url, 'PATCH', data),
-  delete: (url: string) => request(url, 'DELETE'),
+  post: <T, D>(url: string, data: D) => request<T, D>(url, 'POST', data),
+  patch: <T, D>(url: string, data: D) => request<T, D>(url, 'PATCH', data),
+  delete: <T>(url: string) => request<T>(url, 'DELETE'),
 };
