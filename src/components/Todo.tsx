3fd4a1e11ecf91beba2de09f3bfc78dee392128c
nazaromelnickij <nazarjesus228@gmail.com>
2026-06-01 16:59:07 +0300

    solution

---
+++ src/components/Todo.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import classNames from 'classnames';
+import { Todo as TodoType } from '../types/Todo';
+
+type Props = {
+  todo: TodoType;
+  isLoading?: boolean;
+};
+
+export const Todo: React.FC<Props> = ({ todo, isLoading = false }) => {
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames('todo', { completed: todo.completed })}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          readOnly
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+
+      <div
+        data-cy="TodoLoader"
+        className={classNames('modal overlay', { 'is-active': isLoading })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
