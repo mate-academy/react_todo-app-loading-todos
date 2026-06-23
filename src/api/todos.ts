bc9786d4394a16ac115aa1ba7d8e148b63d0e8cd
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-06-23 19:07:21 +0200

    feat: the data is loading

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,13 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 4317;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
 // Add more methods here
+export const deleteData = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
