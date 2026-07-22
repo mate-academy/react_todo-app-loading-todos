594b6b0e9ab9656c8d2f3ea765ba49e5e7f28177
Kseniia <kseniia.vasylenko11@gmail.com>
2026-07-22 11:52:32 +0300

    add some changes

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+
+interface Props {
+  todos: Todo[];
+  tempTodo: Todo | null;
+  loadingTodoIds: number[];
+  onDeleteTodo: (id: number) => void;
+}
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  tempTodo,
+  loadingTodoIds,
+  onDeleteTodo,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          isLoading={loadingTodoIds.includes(todo.id)}
+          onDelete={onDeleteTodo}
+        />
+      ))}
+
+      {tempTodo && <TodoItem todo={tempTodo} isLoading />}
+    </section>
+  );
+};
