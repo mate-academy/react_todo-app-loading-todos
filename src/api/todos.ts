080569c3c3cb2f08ace6c1a84e62e878ece0abd7
nikolamla <nikolamladenovic95@gmail.com>
2026-08-25 21:59:31 +0200

    Solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,7 +1,9 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+// replaced with the userId you received after registering at
+// https://mate-academy.github.io/react_student-registration/
+export const USER_ID = 4436;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
