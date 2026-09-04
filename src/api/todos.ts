729e84f5334633427f5752de99b922a6ca5f30b0
Roman Yasinskiy <4yk.roman@gmail.com>
2026-09-04 17:27:16 +0300

    first part of todo app

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,25 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+//https://mate.academy/students-api/todos?userId=4445
+export const USER_ID = 4445;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+export const addTodo = (title: string) => {
+  return client.post<Todo>('/todos', {
+    userId: USER_ID,
+    title,
+    completed: false,
+  });
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
+
+export const updateTodo = (id: number, data: Partial<Todo>) => {
+  return client.patch<Todo>(`/todos/${id}`, data);
+};
